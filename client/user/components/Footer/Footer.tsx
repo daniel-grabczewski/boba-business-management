2f91a4835255fe38c0ef8cfd48967597a441f766
Rigelle Cruz <rigellecruz@gmail.com>
2023-11-11 11:16:20 +1300

    clean component code for footer

--- client/user/components/Footer/Footer.tsx
+++ client/user/components/Footer/Footer.tsx
@@ -18,37 +18,19 @@ const Footer = () => {
   }
 
   return (
-    <footer className="text-white py-2" style={{ background: '#9B99FF' }}>
-      <div className="flex justify-end pr-12">
-        <div className="space-x-4">
-          <div className="flex space-x-6 text-white">
-            <button
-              className="hover:text-rose-200 transition-colors duration-300"
-              onClick={() => goTo('/')}
-            >
-              Home
-            </button>
-            <button
-              className="hover:text-rose-200 transition-colors duration-300"
-              onClick={() => goTo('/shop')}
-            >
-              Shop
-            </button>
-            <button
-              className="hover:text-rose-200 transition-colors duration-300"
-              onClick={() => goTo('/contact')}
-            >
-              Contact
-            </button>
-            <button
-              className="hover:text-rose-200 transition-colors duration-300"
-              onClick={handleLoginClick}
-            >
-              Admin
-            </button>
-          </div>
-        </div>
-      </div>
+    <footer className="footer">
+      <button className="footer-button" onClick={() => goTo('/')}>
+        Home
+      </button>
+      <button className="footer-button" onClick={() => goTo('/shop')}>
+        Shop
+      </button>
+      <button className="footer-button" onClick={() => goTo('/contact')}>
+        Contact
+      </button>
+      <button className="footer-button" onClick={handleLoginClick}>
+        Admin
+      </button>
     </footer>
   )
 }
