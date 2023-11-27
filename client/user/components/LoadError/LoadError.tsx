461fbd9b5a8dbb56a25f0e690289915fa68a4b15
Rigelle Cruz <rigellecruz@gmail.com>
2023-11-28 09:53:21 +1300

    update loadError component

--- client/user/components/LoadError/LoadError.tsx
+++ client/user/components/LoadError/LoadError.tsx
@@ -16,21 +16,11 @@ function LoadError({ status }: LoadErrorProps) {
     : status
 
   if (combinedStatus === 'loading') {
-    return (
-      <></>
-      // <div className="fixed top-4 mt-4 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white px-4 py-2 rounded shadow-md z-50">
-      //   Loading...
-      // </div>
-    )
+    return <></>
   }
 
   if (combinedStatus === 'error') {
-    return (
-      <></>
-      // <div className="fixed top-4 mt-4 left-1/2 transform -translate-x-1/2 bg-red-500 text-white px-4 py-2 rounded shadow-md z-50">
-      //   An error occurred
-      // </div>
-    )
+    return <></>
   }
 
   return null
