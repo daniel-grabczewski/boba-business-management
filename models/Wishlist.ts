baa60d7c0f58a349343c443b48c385d223cc60e9
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-02 10:21:17 +1200

    convert Wishlist Zod Schemas to Typescript interfaces

--- models/Wishlist.ts
+++ models/Wishlist.ts
@@ -1,11 +1,7 @@
-import * as z from 'zod'
-
-export const wishlistProductSchema = z.object({
-  id: z.number(),
-  productId: z.number(),
-  productName: z.string(),
-  productImage: z.string(),
-  productPrice: z.number(),
-})
-
-export type WishlistProduct = z.infer<typeof wishlistProductSchema>
+export interface WishlistProduct {
+  id: number
+  productId: number
+  productName: string
+  productImage: string
+  productPrice: number
+}
