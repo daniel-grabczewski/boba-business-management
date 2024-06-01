8c484de625cff85e0c07f406598590e8e18dd627
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-02 10:17:37 +1200

    convert Purchases Zod Schemas to Typescript interfaces

--- models/Purchases.ts
+++ models/Purchases.ts
@@ -1,53 +1,42 @@
-import * as z from 'zod'
+export interface UserOrders {
+  orderId: number
+  purchasedAt: string
+  totalAmount: number
+}
 
-export const userOrdersSchema = z.object({
-  orderId: z.number(),
-  purchasedAt: z.string(),
-  totalAmount: z.number(),
-})
+export interface Orders {
+  userName: string
+  orderId: number
+  totalSale: number
+  purchasedAt: string
+  shippingPrice: number
+}
 
-export const ordersSchema = z.object({
-  userName: z.string(),
-  orderId: z.number(),
-  totalSale: z.number(),
-  purchasedAt: z.string(),
-  shippingPrice: z.number(),
-})
+export interface Order {
+  orderId: number
+  userFirstName: string
+  userLastName: string
+  userAddress: string
+  userCity: string
+  userCountry: string
+  userZipCode: string
+  userEmail: string
+  userPhoneNumber: string
+  totalSale: number
+  amountOfItems: number
+  orderDate: string
+  shippingType: string
+  shippingPrice: number
+  orderItems: {
+    productName: string
+    productSale: number
+    productImage: string
+    itemQuantity: number
+  }[]
+}
 
-export const orderSchema = z.object({
-  orderId: z.number(),
-  userFirstName: z.string(),
-  userLastName: z.string(),
-  userAddress: z.string(),
-  userCity: z.string(),
-  userCountry: z.string(),
-  userZipCode: z.string(),
-  userEmail: z.string(),
-  userPhoneNumber: z.string(),
-  totalSale: z.number(),
-  amountOfItems: z.number(),
-  orderDate: z.string(),
-  shippingType: z.string(),
-  shippingPrice: z.number(),
-  orderItems: z
-    .object({
-      productName: z.string(),
-      productSale: z.number(),
-      productImage: z.string(),
-      itemQuantity: z.number(),
-    })
-    .array(),
-})
-
-export const transferedCartSchema = z
-  .object({
-    productId: z.number(),
-    quantity: z.number(),
-    shippingId: z.number(),
-  })
-  .array()
-
-export type UserOrders = z.infer<typeof userOrdersSchema>
-export type Orders = z.infer<typeof ordersSchema>
-export type Order = z.infer<typeof orderSchema>
-export type TransferedCart = z.infer<typeof transferedCartSchema>
+export type TransferedCart = {
+  productId: number
+  quantity: number
+  shippingId: number
+}[]
