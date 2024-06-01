3aa0fa4fa3167d000edcb2e8a8f0c0f43a10339e
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-02 10:20:46 +1200

    convert Users Zod Schemas to Typescript interfaces

--- models/Users.ts
+++ models/Users.ts
@@ -1,40 +1,33 @@
-import * as z from 'zod'
+export interface User {
+  auth0Id: string
+  firstName: string
+  lastName: string
+  userName: string
+  phoneNumber: string
+  emailAddress: string
+  address: string
+  city: string
+  country: string
+  zipCode: string
+}
 
-export const userSchema = z.object({
-  auth0Id: z.string(),
-  firstName: z.string(),
-  lastName: z.string(),
-  userName: z.string(),
-  phoneNumber: z.string(),
-  emailAddress: z.string().email(),
-  address: z.string(),
-  city: z.string(),
-  country: z.string(),
-  zipCode: z.string(),
-})
+export interface UpdateUser {
+  firstName: string
+  lastName: string
+  phoneNumber: string
+  address: string
+  city: string
+  country: string
+  zipCode: string
+}
 
-export const updateUserSchema = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
-  phoneNumber: z.string(),
-  address: z.string(),
-  city: z.string(),
-  country: z.string(),
-  zipCode: z.string(),
-})
+export interface NewUser {
+  firstName: string
+  lastName: string
+  userName: string
+  emailAddress: string
+}
 
-export const newUserSchema = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
-  userName: z.string(),
-  emailAddress: z.string().email(),
-})
-
-export const newUserBackendSchema = newUserSchema.extend({
-  auth0Id: z.string(),
-})
-
-export type User = z.infer<typeof userSchema>
-export type UpdateUser = z.infer<typeof updateUserSchema>
-export type NewUser = z.infer<typeof newUserSchema>
-export type NewUserBackend = z.infer<typeof newUserBackendSchema>
+export interface NewUserBackend extends NewUser {
+  auth0Id: string
+}
