d4ada4d841d9428eb48972f9f2966ae2d7d8ec8d
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-06 10:54:21 +1200

    set up cart data file

---
+++ client/data/cartData.ts
@@ -0,0 +1,6 @@
+import { CartItemInitial } from "../../models/Cart"
+
+//data is empty until user adds their items to their own cart
+const cart: CartItemInitial[] = [] as CartItemInitial[]
+
+export default cart
