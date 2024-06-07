11a289a3c6a2aab330af0e563e70fe07cddeba94
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-08 10:42:04 +1200

    improve clarity of comments

--- client/services/products.ts
+++ client/services/products.ts
@@ -15,20 +15,15 @@ import initialProducts from '../data/productsData'
 
 // Retrieve array of objects 'products' from localStorage
 function getProductsFromLocalStorage(): AdminProduct[] {
-  const products = localStorage.getItem('cart')
+  const products = localStorage.getItem('products')
   return products ? JSON.parse(products) : []
 }
 
-// This will run in App.tsx or index.tsx in a useEffect, essentially running on app startup.
+// If localStorage is empty, initialize localStorage to be initialProducts
 function setProductsInLocalStorageInitial(): void {
-  // Check what is currently inside localStorage for array of objects 'products'
   const productsInStorage = localStorage.getItem('products')
 
-  // If localStorage is empty, initialize localStorage to be initialProducts
   if (!productsInStorage) {
     localStorage.setItem('products', JSON.stringify(initialProducts))
   }
-
-  // Through this condition, this function will only execute once when the app is first loaded. It will only run again if the user clears their browser cache.
-  // So, the Admin's modified/added products will never be replaced by the initialProducts
 }
