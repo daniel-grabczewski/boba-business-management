8ff02b8d325c3514a4f28e9d95dfad38a34c5c47
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-11 11:57:00 +1200

    create generateCurrentDate

--- client/utils/generateDate.ts
+++ client/utils/generateDate.ts
@@ -11,3 +11,14 @@ export function generateCurrentDateTime(): string {
 
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
 }
+
+// Returns the current date in 'YYYY-MM-DD' format
+export function generateCurrentDate(): string {
+  const now = new Date()
+
+  const year = now.getFullYear()
+  const month = String(now.getMonth() + 1).padStart(2, '0') // Months are zero-based
+  const day = String(now.getDate()).padStart(2, '0')
+
+  return `${year}-${month}-${day}`
+}
