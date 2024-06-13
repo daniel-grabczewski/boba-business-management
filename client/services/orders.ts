b954c36b6a63a70bf3c1f3d1023d19bd2be5bd2b
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-14 11:18:03 +1200

    create setOrdersInLocalStorageInitial service function

--- client/services/orders.ts
+++ client/services/orders.ts
@@ -1,13 +1,31 @@
+import initialOrders from '../data/ordersData'
 
-
-//NEEDED: 
+//NEEDED:
 //!(are some of these more appropriate to be written in the cart services?)
-// transferDemoUserCartToOrders (copies the demo user's cart into the orders data, then clears the cart of the demo user)
+//setOrdersInLocalStorageInitial
+//setOrdersInLocalStorage
+//getOrdersFromLocalStorage
+// transferDemoUserCartToOrders(cart) (copies the demo user's cart into the orders data, then clears the cart of the demo user)
 // getLatestOrderIdOfDemoUser (returns the latest orderId from the order the demo user made)
 // getOrderCountFromDate (gets count of the amount of orders that were made on the given date 'DD/MM/YYYY' format)
 // getOrdersOfDemoUser (gets all orders of demo user, with interface of UserOrderSummary[])
 // getAllOrders (gets all orders as AdminOrderSummary[])
 // getOrderById (given order id, returns order as Order interface)
+
+// If localStorage 'orders' key doesn't exist, initialize new key 'orders' to be equal to value of initialOrders
+export function setOrdersInLocalStorageInitial(): void {
+  try {
+    const ordersInStorage = localStorage.getItem('orders')
+
+    if (!ordersInStorage) {
+      localStorage.setItem('orders', JSON.stringify(initialOrders))
+    }
+  } catch (error) {
+    console.error('Failed to initialize orders in localStorage:', error)
+  }
+}
+
+
 
 export async function moveCartToPurchases(shippingId: number, token: string) {
   try {
@@ -32,7 +50,7 @@ export async function fetchLatestOrderId(token: string) {
   } catch (error) {
     console.error(
       'Error fetching latest purchase order:',
-      (error as Error).message,
+      (error as Error).message
     )
     throw { error: (error as Error).message }
   }
