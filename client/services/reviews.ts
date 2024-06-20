f1e407499adc84b92615050e79988517d9d7c37d
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-20 12:10:26 +1200

    create getReviewsByProductId service function

--- client/services/reviews.ts
+++ client/services/reviews.ts
@@ -1,5 +1,6 @@
-import { NewReview, UpdatedReviewStatus, Review } from '../../models/Reviews'
+import { NewReview, UpdatedReviewStatus, Review, ProductReview } from '../../models/Reviews'
 import initialReviews from '../data/reviewsData'
+import { getUserByUserId } from './users'
 
 //!What if we have a system where we add a bunch of reviews as soon as the user boots up the app, so they are each added with the current date, with a time that is before the current time. So that way, when the user goes into the admin dashboard, it isn't competely empty.
 
@@ -37,7 +38,38 @@ export function getReviewsFromLocalStorage(): Review[] {
   }
 }
 
-//getReviewsByProductId(productId) - Returns all reviews associated with given product id as ProductReview[]
+// Get all reviews associated with given product id as ProductReview[]
+export function getReviewsByProductId(productId: number): ProductReview[] {
+  try {
+    const reviews = getReviewsFromLocalStorage()
+    const reviewsForProduct = reviews
+      .filter(review => review.productId === productId)
+      .map(({ productId, userId, rating, createdAt, description }) => {
+        try {
+          const userName = getUserByUserId(userId)?.userName || 'Error Retrieving User'
+          return { productId, userName, rating, createdAt, description }
+        } catch (userError) {
+          console.error(`Failed to retrieve user details for userId ${userId}:`, userError)
+          return { productId, userName: 'Error Retrieving User', rating, createdAt, description }
+        }
+      })
+    return reviewsForProduct
+  } catch (error) {
+    console.error('Failed to get reviews for product id:', error)
+    return []
+  }
+}
+
+/*
+export interface ProductReview {
+  productId: number
+  userName: string
+  rating: number
+  createdAt: string
+  description: string
+}
+
+*/
 
 
 //getCountOfReviewsFromDate(DD/MM/YYYY) - Returns count of reviews that were made on given date.
