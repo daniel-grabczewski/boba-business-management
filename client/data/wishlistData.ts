349e122df281b33e65096bf75470a6deede6edeb
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-25 11:30:56 +1200

    remove wishlistData.ts

--- client/data/wishlistData.ts
+++
@@ -1,6 +0,0 @@
-import {WishlistProductInitial} from '../../models/Wishlist'
-
-//data is empty until user adds their items to their own wishlist
-const wishlist: WishlistProductInitial[] = [] as WishlistProductInitial[]
-
-export default wishlist
