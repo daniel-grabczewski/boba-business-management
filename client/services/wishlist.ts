1db585d6270a9b5587f4f27ef8e039433a8fd6d1
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-26 11:27:44 +1200

    create setWishlistItemsInLocalStorage service function

--- client/services/wishlist.ts
+++ client/services/wishlist.ts
@@ -1,8 +1,17 @@
-import { Wishlist, WishlistExtraDetails} from '../../models/Wishlist'
+import { WishlistItem, DisplayWishlistItem} from '../../models/Wishlist'
 
-// localStorage set, get
-// getWishlist() get wishlist as WithlistProductDetail
+// localStorage set, get. Both as Wishlist[]
+// getWishlistWithExtraDetails() get wishlist as WithlistExtraDetails[]
 // generateNewWishlistId()
 // isProductInWishlistByProductId (productId) Return true or false, depending on whether the product associated with given product id is in the demo user's wish list.
 // addProductToWishlistByProductId(productId) add product to wishlist
 // deleteProductFromWishlistByProductId(productId)
+
+// Replace localStorage wishlistItems with given wishlistItems
+export function setWishlistItemsInLocalStorage(wishlistItems: WishlistItem[]): void {
+  try {
+    localStorage.setItem('wishlistItems', JSON.stringify(wishlistItems))
+  } catch (error) {
+    console.error('Failed to set wishlistItems in localStorage:', error)
+  }
+}
