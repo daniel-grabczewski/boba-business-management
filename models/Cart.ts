2bac4d19823666ec09088adc75a11f8cd0bb384f
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-25 12:30:10 +1200

    modify cart interfaces

--- models/Cart.ts
+++ models/Cart.ts
@@ -1,4 +1,6 @@
-export interface CartItem {
+// Represents the detailed data needed for displaying the cart item in a React component
+export interface DisplayCartItem {
+  id : number
   image: string
   name: string
   quantity: number
@@ -7,7 +9,9 @@ export interface CartItem {
   productId: number
 }
 
-export interface CartTransferInfo {
-  userId: string
-  shippingId: number
+// Represents a single cart item within the cart with minimal data needed for storage
+export interface CartItem {
+  id : number
+  productId : number
+  quantity : number
 }
