109f037dcc5a8a2abc1153c35253d8b8c14f1579
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-27 11:12:00 +1200

    modify index.tsx

--- client/index.tsx
+++ client/index.tsx
@@ -6,7 +6,6 @@ import {
   Route,
 } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { Auth0Provider } from '@auth0/auth0-react'
 
 import AppLayout from './user/components/AppLayout/AppLayout'
 import Cart from './user/pages/Cart/Cart'
@@ -23,27 +22,41 @@ import {
   EditProfile,
   Wishlist,
 } from './user/pages/index'
-import { Reviews, Dashboard, Emails, ProductsSummary, IndividualProduct } from './admin/pages/'
+import {
+  Reviews,
+  Dashboard,
+  Emails,
+  ProductsSummary,
+  IndividualProduct,
+} from './admin/pages/'
 
 import Redirect from './user/pages/Redirect/Redirect'
 import AllOrders from './admin/pages/Orders/AllOrders'
-import AdminComponent from './UI/AdminComponent'
 import ProtectedComponent from './UI/ProtectedComponent'
 import AddProduct from './admin/pages/AddProduct/AddProduct'
 
+
 export const routes = createRoutesFromElements(
-  <Route path="/" element={<AppLayout />} errorElement={<ErrorPage />}>
-    <Route index element={<UnprotectedComponent component={Home} />} />
-    <Route path="cart" element={<UnprotectedComponent component={Cart} />} />
-    <Route path="shop" element={<UnprotectedComponent component={Shop} />} />
+  <Route
+    path={
+      process.env.NODE_ENV === 'production'
+        ? '/boba-business-management-demo/'
+        : '/'
+    }
+    element={<AppLayout />}
+    errorElement={<ErrorPage />}
+  >
+    <Route index element={<ProtectedComponent component={Home} />} />
+    <Route path="cart" element={<ProtectedComponent component={Cart} />} />
+    <Route path="shop" element={<ProtectedComponent component={Shop} />} />
     <Route
       path="redirect"
-      element={<UnprotectedComponent component={Redirect} />}
+      element={<ProtectedComponent component={Redirect} />}
     />
 
     <Route
       path="shop/:id"
-      element={<UnprotectedComponent component={ProductPage} />}
+      element={<ProtectedComponent component={ProductPage} />}
     />
     <Route
       path="thankyou"
@@ -71,32 +84,36 @@ export const routes = createRoutesFromElements(
     />
     <Route
       path="admin/reviews"
-      element={<AdminComponent component={Reviews} />}
+      element={<ProtectedComponent component={Reviews} />}
     />
-    <Route path="admin" element={<AdminComponent component={Dashboard} />} />
+    <Route
+      path="admin"
+      element={<ProtectedComponent component={Dashboard} />}
+    />
 
     <Route
       path="admin/orders"
-      element={<AdminComponent component={AllOrders} />}
+      element={<ProtectedComponent component={AllOrders} />}
     />
-    {/* change back to AdminComponent for admin/productsSummary & admin/:id */}
     <Route
       path="admin/products-summary"
-      element={<AdminComponent component={ProductsSummary} />}
+      element={<ProtectedComponent component={ProductsSummary} />}
     />
     <Route
       path="admin/edit/:id"
-      element={<AdminComponent component={IndividualProduct} />}
+      element={<ProtectedComponent component={IndividualProduct} />}
     />
-
 
     <Route
       path="admin/add-product"
-      element={<AdminComponent component={AddProduct} />}
+      element={<ProtectedComponent component={AddProduct} />}
     />
 
-    <Route path="admin/inbox" element={<AdminComponent component={Emails} />} />
-  </Route>,
+    <Route
+      path="admin/inbox"
+      element={<ProtectedComponent component={Emails} />}
+    />
+  </Route>
 )
 
 function AppProvider() {
@@ -106,24 +123,8 @@ function AppProvider() {
 document.addEventListener('DOMContentLoaded', () => {
   const queryClient = new QueryClient()
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <Auth0Provider
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      domain={import.meta.env.VITE_AUTH0_DOMAIN as string}
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID as string}
-      cacheLocation="localstorage"
-      authorizationParams={{
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        audience: import.meta.env.VITE_AUTH0_AUDIENCE as string,
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <QueryClientProvider client={queryClient}>
-        <AppProvider />
-      </QueryClientProvider>
-    </Auth0Provider>,
+    <QueryClientProvider client={queryClient}>
+      <AppProvider />
+    </QueryClientProvider>
   )
 })
