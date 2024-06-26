4e63e4046dd8be6b9f150b199ebdf12f9dbfd3bc
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-27 11:11:40 +1200

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
+// vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import EnvironmentPlugin from 'vite-plugin-environment'
+import dotenv from 'dotenv'
 
-// https://vitejs.dev/config/
+dotenv.config()
+
 export default defineConfig({
-  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/boba-business-management-demo/' : '/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: '404.html', dest: '.' }],
+    }),
+    EnvironmentPlugin({
+      NODE_ENV: process.env.NODE_ENV
+    })
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
