9f78153e2511ecde627490457fc7050379de5e10
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-26 12:04:19 +1200

    create addProductToWishlistItemsByProductId service function

--- client/services/wishlist.ts
+++ client/services/wishlist.ts
@@ -1,11 +1,12 @@
-import { WishlistItem, DisplayWishlistItem} from '../../models/Wishlist'
+import { WishlistItem, DisplayWishlistItem } from '../../models/Wishlist'
 import { getProductByIdAdmin } from './products'
 
-// addProductToWishlistByProductId(productId) add product to wishlist
 // deleteProductFromWishlistByProductId(productId)
 
 // Replace localStorage wishlistItems with given wishlistItems
-export function setWishlistItemsInLocalStorage(wishlistItems: WishlistItem[]): void {
+export function setWishlistItemsInLocalStorage(
+  wishlistItems: WishlistItem[]
+): void {
   try {
     localStorage.setItem('wishlistItems', JSON.stringify(wishlistItems))
   } catch (error) {
@@ -28,21 +29,24 @@ export function getWishlistItemsFromLocalStorage(): WishlistItem[] {
 export function getDisplayWishlistItems(): DisplayWishlistItem[] {
   try {
     const wishlistItems = getWishlistItemsFromLocalStorage()
-    
-    const displayWishlistItems = wishlistItems.map(({productId, id}) => {
-      const product = getProductByIdAdmin(productId)
-      if (product) {
-        return {
-          id,
-          productId,
-          productName: product.name,
-          productImage: product.image ,
-          productPrice: product.price,
+
+    const displayWishlistItems = wishlistItems
+      .map(({ productId, id }) => {
+        const product = getProductByIdAdmin(productId)
+        if (product) {
+          return {
+            id,
+            productId,
+            productName: product.name,
+            productImage: product.image,
+            productPrice: product.price,
+          }
         }
-      }
-      return undefined
-      
-    }).filter(wishlistItem => wishlistItem !== undefined) as DisplayWishlistItem[]
+        return undefined
+      })
+      .filter(
+        (wishlistItem) => wishlistItem !== undefined
+      ) as DisplayWishlistItem[]
 
     return displayWishlistItems
   } catch (error) {
@@ -51,23 +55,52 @@ export function getDisplayWishlistItems(): DisplayWishlistItem[] {
   }
 }
 
-// Generate unique wishlistItem id
-export function generateNewWishlistItemId() : number {
+// Generate unique wishlistItem ID
+export function generateNewWishlistItemId(): number {
   const wishlistItems = getWishlistItemsFromLocalStorage()
   const newId =
-  wishlistItems.length > 0 ? Math.max(...wishlistItems.map((wishlistItems) => wishlistItems.id)) + 1 : 1
+    wishlistItems.length > 0
+      ? Math.max(...wishlistItems.map((wishlistItems) => wishlistItems.id)) + 1
+      : 1
 
   return newId
 }
 
-// Return true or false, depending on whether the product associated with given product id is in the demo user's wish list.
-export function isProductInWishlistItemsByProductId(productId : number) : boolean {
+// Return true or false, depending on whether the product associated with given product ID is in the demo user's wish list.
+export function isProductInWishlistItemsByProductId(
+  productId: number
+): boolean {
   try {
     const wishlistItems = getWishlistItemsFromLocalStorage()
-    const isProductInWishlist = wishlistItems.some(wishlistItem => wishlistItem.productId === productId)
+    const isProductInWishlist = wishlistItems.some(
+      (wishlistItem) => wishlistItem.productId === productId
+    )
     return isProductInWishlist
   } catch (error) {
-    console.error(`Failed to check whether wishlistItems contains product id of ${productId}`, error)
+    console.error(
+      `Failed to check whether wishlist items contains product ID of ${productId}`,
+      error
+    )
     return false
+  }
+}
+
+// Add product to wishlist with given product ID
+export function addProductToWishlistItemsByProductId(productId: number) {
+  try {
+    if (isProductInWishlistItemsByProductId(productId)) {
+      console.log(
+        `Product with ID of ${productId} already exists in wishlistItems`
+      )
+    } else {
+      const wishlistItems = getWishlistItemsFromLocalStorage()
+      wishlistItems.push({ id: generateNewWishlistItemId(), productId })
+      setWishlistItemsInLocalStorage(wishlistItems)
+    }
+  } catch (error) {
+    console.error(
+      `Failed to add product with ID ${productId} to wishlist items`,
+      error
+    )
   }
 }
