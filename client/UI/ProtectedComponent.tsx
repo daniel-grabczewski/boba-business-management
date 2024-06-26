a9c553fb7ab5cc74b37dba74826752f473eb7fed
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-27 10:55:10 +1200

    modify ProtectedComponent.tsx

--- client/UI/ProtectedComponent.tsx
+++ client/UI/ProtectedComponent.tsx
@@ -1,38 +1,12 @@
-import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
-import { useQuery } from 'react-query'
-import { fetchCheckIfUserExists } from '../services/users'
-import LoadError from '../user/components/LoadError/LoadError'
+import React from 'react'
 
 interface Props {
   component: React.ComponentType<unknown>
 }
 
-// This is a very handy wrapper component that will redirect the user to the login page
-// if they are not logged in
 export const ProtectedComponent = ({ component }: Props) => {
-  const { getAccessTokenSilently } = useAuth0()
-  const { data: ifUserExists, status: statusUserExists } = useQuery(
-    ['fetchCheckIfUserExists'],
-    async () => {
-      const token = await getAccessTokenSilently()
-      return await fetchCheckIfUserExists(token)
-    }
-  )
-  const Component = withAuthenticationRequired(component, {
-    onRedirecting: () => (
-      <div>
-        <h1>Redirecting you to the login page...</h1>
-      </div>
-    ),
-  })
-
-  return (
-    <>
-      <LoadError status={statusUserExists} />
-
-      {/* will create a new page for asking the user to create a new account */}
-      {ifUserExists ? <Component /> : <div>Create an account to continue</div>}
-    </>
-  )
+  const Component = component
+  return <Component />
 }
+
 export default ProtectedComponent
