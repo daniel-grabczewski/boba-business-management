709b6e1edc1c9024919ff0e44a2443c192c9370f
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-07-01 12:07:46 +1200

    Modify AppLayout component

--- client/user/components/AppLayout/AppLayout.tsx
+++ client/user/components/AppLayout/AppLayout.tsx
@@ -1,36 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Nav from '../Nav/Nav'
 import Footer from '../Footer/Footer'
-import { useQuery } from 'react-query'
-import { fetchIsUserAdmin } from '../../../services/users'
-import { User } from '../../../../models/Users'
 import { AdminNavBar } from '../../../admin/components/AdminNavBar/AdminNavBar'
-import LoadError from '../LoadError/LoadError'
-import { useAuth0 } from '@auth0/auth0-react'
 
 function AppLayout() {
-  const { getAccessTokenSilently } = useAuth0()
-  const { data: isAdmin, status: statusIsAdmin } = useQuery(
-    ['fetchIsUserAdmin'],
-    async () => {
-      const token = await getAccessTokenSilently()
-      const isAdmin: boolean = await fetchIsUserAdmin(token)
-      return isAdmin
-    }
-  )
-
   return (
     <div className="flex flex-col min-h-screen">
-      <LoadError status={statusIsAdmin} />
-
-      {isAdmin ? <AdminNavBar /> : <Nav />}
-
+      <AdminNavBar />
+      <Nav />
       <div className="flex-grow">
         <Outlet />
       </div>
 
-      {/* Footer */}
-      {isAdmin ? <Footer /> : <Footer />}
+      <Footer />
     </div>
   )
 }
