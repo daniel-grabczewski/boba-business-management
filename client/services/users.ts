0073e308323a5d962dc489e375ba62eab941d1e6
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-07-01 12:07:32 +1200

    remove unneeded models from users services

--- client/services/users.ts
+++ client/services/users.ts
@@ -1,5 +1,5 @@
 
-import { NewUser, UpdateUser, User } from '../../models/Users'
+import {User} from '../../models/Users'
 import initialUsers from '../data/usersData'
 
 // Initialize new key 'users' to be equal to value of initialUsers IF localStorage 'users' key doesn't exist
@@ -50,76 +50,3 @@ export function getUserByUserId(userId: string): User | null {
     return null
   }
 }
-
-
-/*
-export async function fetchUser(token: string) {
-  try {
-    const response = await request
-      .get(`${baseUrl}`)
-      .set('Authorization', `Bearer ${token}`)
-
-    const userData = response.body as User
-    return userData
-  } catch (error) {
-    console.error('An error occurred:', (error as Error).message)
-    throw { error: (error as Error).message }
-  }
-}
-
-export async function fetchCheckIfUserExists(token: string) {
-  try {
-    const response = await request
-      .get(`${baseUrl}/check`)
-      .set('Authorization', `Bearer ${token}`)
-    return response.body
-  } catch (error) {
-    console.error('An error occurred:', (error as Error).message)
-    throw { error: (error as Error).message }
-  }
-}
-
-
-export async function fetchIsUserAdmin(token: string) {
-  try {
-    const response = await request
-      .get(`${baseUrl}/isAdmin`)
-      .set('Authorization', `Bearer ${token}`)
-    const isAdmin: boolean = response.body
-    return isAdmin
-  } catch (error) {
-    console.error('An error occurred:', (error as Error).message)
-    throw { error: (error as Error).message }
-  }
-}
-
-export async function modifyUserDetails(
-  updatedUser: UpdateUser,
-  token: string,
-) {
-  try {
-    await request
-      .patch(`${baseUrl}/edit`)
-      .set('Authorization', `Bearer ${token}`)
-      .send(updatedUser)
-  } catch (error) {
-    console.error('Error modifying user details:', (error as Error).message)
-    return { error: (error as Error).message }
-  }
-}
-
-export async function insertUser(
-  newUser: NewUser,
-  token: string,
-) {
-  try {
-    await request
-      .post(`${baseUrl}/`)
-      .set('Authorization', `Bearer ${token}`)
-      .send(newUser)
-  } catch (error) {
-    console.error('Error modifying user details:', (error as Error).message)
-    return { error: (error as Error).message }
-  }
-}
-  */
