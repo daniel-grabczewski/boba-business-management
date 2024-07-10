d9af6cd6b222843006e60338cbe80afdb23282f8
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-07-10 13:11:40 +1200

    Delete PaymentInformation component
    Functionality was moved to the DeliveryAddress component

--- client/shopper/components/index.ts
+++ client/shopper/components/index.ts
@@ -1,11 +1,9 @@
 import DeliveryAddress from './Checkout/DeliveryAddress'
 import OrderSummary from './Checkout/OrderSummary'
-import PaymentInformation from './Checkout/PaymentInformation'
 import PaymentMethod from './Checkout/PaymentMethod'
 import ShippingMethod from './Checkout/ShippingMethod'
 
 export {
-  PaymentInformation,
   DeliveryAddress,
   PaymentMethod,
   ShippingMethod,
