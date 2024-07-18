96f60d1a0a510426a5f3b5139769cebeea90d9d1
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-07-19 11:18:55 +1200

    Only show load cart if there it contains items

--- client/shopper/pages/Cart/Cart.tsx
+++ client/shopper/pages/Cart/Cart.tsx
@@ -62,6 +62,18 @@ const Cart = () => {
   return (
     <>
       <LoadError status={status} />
+      {data?.length === 0 ? (
+        <div className="flex flex-col items-center justify-center mt-12">
+        <h1 className="text-3xl font text-center mt-12 mb-4">
+          {`No items currently in your cart. Let's get shopping! 😀`}
+        </h1>
+        <button className="bg-gray-500 text-white p-4 w-auto whitespace-nowrap text-lg font-bold rounded-md transition-colors hover:bg-gray-600 focus:outline-none focus:ring focus:ring-gray-300 mt-4"
+        onClick={() => navigate('/shop')}
+        >
+          Continue shopping
+        </button>
+      </div>
+      ) :
       <div className="flex justify-center items-center min-h-screen mt-4">
         <div className="flex w-3/5 justify-end">
           <div className="w-3/5 pl-6">
@@ -166,6 +178,7 @@ const Cart = () => {
           </div>
         </div>
       </div>
+}
     </>
   )
 }
