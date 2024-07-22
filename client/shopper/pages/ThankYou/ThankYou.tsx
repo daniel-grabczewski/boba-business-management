53491cc3066c450a12968493bd3b2e02f1217b41
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-07-22 12:22:44 +1200

    Modify ThankYou page to properly display details of latest order made by demo user

--- client/shopper/pages/ThankYou/ThankYou.tsx
+++ client/shopper/pages/ThankYou/ThankYou.tsx
@@ -1,44 +1,39 @@
 import { useQuery } from 'react-query'
-import { getDemoUserDetails } from '../../../services/users'
 import LoadError from '../../components/LoadError/LoadError'
-import { getIdOfLatestOrderDemoUser } from '../../../services/orders'
+import { getLatestOrderOfDemoUser } from '../../../services/orders'
 import { useNavigate } from 'react-router-dom'
 
 const ThankYou = () => {
   const navigate = useNavigate()
 
-  const { data: user, status } = useQuery(['getUser'], async () => {
-    return getDemoUserDetails()
-  })
-
-  const { data: orderId, status: statusOrderId } = useQuery(
-    ['getLatestOrderId'],
-    async () => getIdOfLatestOrderDemoUser()
+  const { data: demoUserOrder, status: statusDemoUserOrder } = useQuery(
+    ['getLatestOrderOfDemoUser'],
+    async () => getLatestOrderOfDemoUser()
   )
 
+
   return (
-    //! NEW ISSUE, THE ADDRESS OF DELIVERY SHOULD BE OF THE ORDER, NOT THE DEFAULT USER'S ADDRESS
     <>
-      <LoadError status={[status, statusOrderId]} />
+      <LoadError status={statusDemoUserOrder} />
       <div className="mt-8 text-2xl text-center font-bold">
         <div>THANK YOU</div>
-        {user && <div>{`${user.firstName} ${user.lastName}`}</div>}
+        {demoUserOrder && <div>{`${demoUserOrder.firstName} ${demoUserOrder.lastName}`}</div>}
         <div>FOR YOUR ORDER!</div>
       </div>
 
-      {orderId && (
+      {demoUserOrder && (
         <div className="mt-8 mb-8 ml-2 border rounded-md px-3 py-2 text-center">
-          ORDER NUMBER #{orderId}
+          ORDER NUMBER #{demoUserOrder.id}
         </div>
       )}
 
       <div className="mt-8 mb-8 ml-2 border rounded-md px-3 py-2 text-center">
         <div className="text-xl">Your order will be shipped to</div>
-        {user && (
+        {demoUserOrder && (
           <div>
-            <div>{user.address}</div>
-            <div>{user.city}</div>
-            <div>{user.country}</div>
+            <div>{demoUserOrder.address}</div>
+            <div>{demoUserOrder.city}</div>
+            <div>{demoUserOrder.country}, {demoUserOrder.zipCode}</div>
           </div>
         )}
       </div>
