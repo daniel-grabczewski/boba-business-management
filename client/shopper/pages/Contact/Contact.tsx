f226e8678191d3d6b21550bb5fac6ee402004121
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-07-25 11:21:48 +1200

    Improve contact page UI

--- client/shopper/pages/Contact/Contact.tsx
+++ client/shopper/pages/Contact/Contact.tsx
@@ -4,7 +4,7 @@ import { NewEmail } from '../../../../models/Emails'
 import { sendEmailFromDemoUser } from '../../../services/emails'
 
 function Contact() {
-  const mutations = useMutation(async (newEmail: NewEmail) => 
+  const mutations = useMutation(async (newEmail: NewEmail) =>
     sendEmailFromDemoUser(newEmail)
   )
 
@@ -18,7 +18,6 @@ function Contact() {
     const { name, value } = event.target
 
     setNewEmail((prevEmail) => ({ ...prevEmail, [name]: value }))
-
   }
 
   function handleMessageChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
@@ -53,7 +52,7 @@ function Contact() {
   }, [alertMessage])
 
   return (
-    <div className="mx-auto max-w-4xl p-8 min-h-screen">
+    <div className="mx-auto max-w-2xl p-8 flex flex-col">
       <div className="text-3xl text-center font-bold mb-6">Contact Us</div>
       {alertMessage && (
         <div className="bg-green-200 text-green-800 p-2 mb-4 rounded">
@@ -80,6 +79,7 @@ function Contact() {
           </label>
           <textarea
             className="mt-2 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-black w-full"
+            style={{ height: '150px', maxHeight: '300px', minHeight: '150px' }}
             name="description"
             onChange={handleMessageChange}
             value={newEmail.description}
@@ -88,26 +88,16 @@ function Contact() {
             required
           />
         </div>
-        <button
-          className="bg-black text-white py-2 px-4 rounded-md w-full text-lg font-semibold hover:bg-gray-800 hover:text-gray-100 transition-all duration-300"
-          type="button"
-          onClick={handleSubmit}
-        >
-          Submit
-        </button>
+        <div className="w-1/4 mx-auto">
+          <button
+            className="bg-black text-white py-2 px-4 rounded-md w-full text-lg font-semibold hover:bg-gray-800 hover:text-gray-100 transition-all duration-300"
+            type="button"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
       </form>
-      <div className="mt-8">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.06300832411!2d174.7764339931746!3d-36.8649106243409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47c43b7c00a3%3A0x4abe94b659ab7fb0!2sDev%20Academy%20Aotearoa%20-%20Auckland%20Campus!5e0!3m2!1sen!2snz!4v1695091841249!5m2!1sen!2snz"
-          title="map"
-          width="830"
-          height="400"
-          style={{ border: 0 }}
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-        <p>(this address is not relevant to this website)</p>
-      </div>
     </div>
   )
 }
