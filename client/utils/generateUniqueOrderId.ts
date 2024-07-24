63eab8b97a9a36b604dbff03680c2545ae1ff65a
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-07-25 10:18:12 +1200

    Improve unique order ID generation

--- client/utils/generateUniqueOrderId.ts
+++ client/utils/generateUniqueOrderId.ts
@@ -12,9 +12,9 @@ export function generateUniqueOrderId(dateString?: string): number {
   let date: Date
 
   if (dateString) {
-    date = parseDateString(dateString)
+      date = parseDateString(dateString)
   } else {
-    date = new Date()
+      date = new Date()
   }
 
   const year = date.getFullYear().toString().slice(-2)
@@ -23,21 +23,18 @@ export function generateUniqueOrderId(dateString?: string): number {
   const hours = date.getHours().toString().padStart(2, '0')
   const minutes = date.getMinutes().toString().padStart(2, '0')
   const seconds = date.getSeconds().toString().padStart(2, '0')
+  const milliseconds = date.getMilliseconds().toString().padStart(3, '0')
 
-  const baseString = `${year}${month}${day}${hours}${minutes}${seconds}`
+  const baseString = `${year}${month}${day}${hours}${minutes}${seconds}${milliseconds}`
 
   let scrambledString = ''
-  const scrambleOrder = [2, 5, 3, 8, 1, 7]
+  const scrambleOrder = [2, 5, 8, 11, 1, 9] 
   scrambleOrder.forEach((index) => {
-    scrambledString += baseString[index]
+      scrambledString += baseString[index]
   })
 
-  if (scrambledString.length < 6) {
-    scrambledString = scrambledString.padEnd(6, '0')
-  }
-
   if (scrambledString.startsWith('0')) {
-    scrambledString = '1' + scrambledString.slice(1)
+      scrambledString = '1' + scrambledString.slice(1)
   }
 
   return Number(scrambledString)
