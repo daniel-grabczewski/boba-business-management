30f4a73f17e8407877326ae83a09f7a9134a6dcd
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-05 12:58:23 +1200

    Fix bug in ViewProducts component where low stock products had a larger container than products with normal stock

--- client/admin/components/ViewProducts/ViewProducts.tsx
+++ client/admin/components/ViewProducts/ViewProducts.tsx
@@ -18,124 +18,121 @@ const ViewProducts = ({
       {getPaginatedProducts().length === 0 ? (
         <p className="text-center mt-4 font-semi-bold">No products found</p>
       ) : (
-        <div className="flex flex-col flex-wrap">
+        <div className="flex flex-col flex-wrap gap-4">
           {getPaginatedProducts().map((product) => (
             <div
               key={product.id}
-              className={(product.stock < 5 && 'border-4 border-red-600') || ''}
-              style={{ width: 'auto' }}
+              className={`box-border rounded-md flex flex-row items-center justify-between ${
+                product.stock < 5
+                  ? 'border-2 border-red-600'
+                  : 'border-2 border-gray-200'
+              }`}
+              style={{ width: '750px', height: '200px' }}
             >
-              <div
-                key={product.id}
-                className="border p-4 rounded-md flex flex-row items-center justify-between"
-                style={{ width: '750px' }}
-              >
-                <div className="w-1/4">
-                  <Link
-                    to={`/admin/edit/${product.id}`}
-                    className="w-full h-48 block"
-                    // style={{ marginBottom: "15px" }}
-                  >
-                    <img
-                      src={product.image}
-                      alt={product.name}
-                      className="w-full h-48 object-contain"
-                    />
-                  </Link>
-                </div>
+              <div className="w-1/4">
+                <Link
+                  to={`/admin/edit/${product.id}`}
+                  className="w-full h-48 block"
+                  // style={{ marginBottom: "15px" }}
+                >
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    className="h-48"
+                    style={{ width: '150px' }}
+                  />
+                </Link>
+              </div>
 
-                <div className="flex-col w-1/4 mr-6">
+              <div className="flex-col w-1/4 mr-6">
+                <Link
+                  to={`/admin/edit/${product.id}`}
+                  onMouseEnter={() => setHoveredProductId(product.id)}
+                  onMouseLeave={() => setHoveredProductId(null)}
+                  className="text-2xl font-bold mt-2 block cursor-pointer"
+                  style={{
+                    color:
+                      hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
+                  }}
+                >
+                  {product.name}
+                </Link>
+
+                <Link
+                  to={`/admin/edit/${product.id}`}
+                  className="block cursor-pointer"
+                >
+                  <div className="flex items-center mt-2">
+                    <span className="text-yellow-400">
+                      <StarRating rating={product.averageRating} size={1.5} />
+                    </span>
+                    <span className="ml-2 text-sm text-gray-500">
+                      ({product.averageRating})
+                    </span>
+                  </div>
+                </Link>
+              </div>
+
+              <div className="w-1/5">
+                <Link
+                  to={`/admin/edit/${product.id}`}
+                  onMouseEnter={() => setHoveredProductId(product.id)}
+                  onMouseLeave={() => setHoveredProductId(null)}
+                  className="text-xl font-bold block cursor-pointer"
+                  style={{
+                    color:
+                      hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
+                  }}
+                >
+                  ${product.price.toFixed(2)}
+                </Link>
+              </div>
+
+              <div className="w-1/5">
+                <Link
+                  to={`/admin/edit/${product.id}`}
+                  onMouseEnter={() => setHoveredProductId(product.id)}
+                  onMouseLeave={() => setHoveredProductId(null)}
+                  className="text-xl font-bold block cursor-pointer"
+                  style={{
+                    color:
+                      hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
+                  }}
+                >
+                  {`Stock: ${product.stock}`}
+                </Link>
+              </div>
+
+              <div className="flex-col w-1/4">
+                {product.stock < 5 && (
                   <Link
                     to={`/admin/edit/${product.id}`}
                     onMouseEnter={() => setHoveredProductId(product.id)}
                     onMouseLeave={() => setHoveredProductId(null)}
-                    className="text-2xl font-bold mt-2 block cursor-pointer"
+                    className="text-xl font-bold mt-2 block cursor-pointer pb-24"
                     style={{
                       color:
                         hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
                     }}
                   >
-                    {product.name}
+                    <span className="text-red-500">LOW STOCK</span>
                   </Link>
+                )}
 
-                  <Link
-                    to={`/admin/edit/${product.id}`}
-                    className="block cursor-pointer"
-                  >
-                    <div className="flex items-center mt-2">
-                      <span className="text-yellow-400">
-                        <StarRating rating={product.averageRating} size={1.5} />
-                      </span>
-                      <span className="ml-2 text-sm text-gray-500">
-                        ({product.averageRating})
-                      </span>
-                    </div>
-                  </Link>
-                </div>
-
-                <div className="w-1/5">
-                  <Link
-                    to={`/admin/edit/${product.id}`}
-                    onMouseEnter={() => setHoveredProductId(product.id)}
-                    onMouseLeave={() => setHoveredProductId(null)}
-                    className="text-xl font-bold block cursor-pointer"
-                    style={{
-                      color:
-                        hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
-                    }}
-                  >
-                    ${product.price.toFixed(2)}
-                  </Link>
-                </div>
-
-                <div className="w-1/5">
-                  <Link
-                    to={`/admin/edit/${product.id}`}
-                    onMouseEnter={() => setHoveredProductId(product.id)}
-                    onMouseLeave={() => setHoveredProductId(null)}
-                    className="text-xl font-bold block cursor-pointer"
-                    style={{
-                      color:
-                        hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
-                    }}
-                  >
-                    {`Stock: ${product.stock}`}
-                  </Link>
-                </div>
-
-                <div className="flex-col w-1/4">
-                  {product.stock < 5 && (
-                    <Link
-                      to={`/admin/edit/${product.id}`}
-                      onMouseEnter={() => setHoveredProductId(product.id)}
-                      onMouseLeave={() => setHoveredProductId(null)}
-                      className="text-xl font-bold mt-2 block cursor-pointer pb-24"
-                      style={{
-                        color:
-                          hoveredProductId === product.id
-                            ? '#1D4ED8'
-                            : 'inherit',
-                      }}
-                    >
-                      <span className="text-red-500">LOW STOCK</span>
-                    </Link>
-                  )}
-
-                  <Link
-                    to={`/admin/edit/${product.id}`}
-                    onMouseEnter={() => setHoveredProductId(product.id)}
-                    onMouseLeave={() => setHoveredProductId(null)}
-                    className="text-xl font-bold block cursor-pointer"
-                    style={{
-                      color:
-                        hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
-                    }}
-                  >
-                    {(product.isEnabled && (
-                      <span className="text-green-600">ENABLED</span>
-                    )) || <span className="text-red-600">DISABLED</span>}
-                  </Link>
-                </div>
+                <Link
+                  to={`/admin/edit/${product.id}`}
+                  onMouseEnter={() => setHoveredProductId(product.id)}
+                  onMouseLeave={() => setHoveredProductId(null)}
+                  className="text-xl font-bold block cursor-pointer"
+                  style={{
+                    color:
+                      hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
+                  }}
+                >
+                  {(product.isEnabled && (
+                    <span className="text-green-600">ENABLED</span>
+                  )) || <span className="text-red-600">DISABLED</span>}
+                </Link>
               </div>
             </div>
           ))}
