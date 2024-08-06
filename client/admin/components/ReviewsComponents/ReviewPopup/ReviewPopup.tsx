6e32d35ffce1360ce2f61ea8240a6b405ef68745
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-07 11:21:13 +1200

    Improve UI of review popup

--- client/admin/components/ReviewsComponents/ReviewPopup/ReviewPopup.tsx
+++ client/admin/components/ReviewsComponents/ReviewPopup/ReviewPopup.tsx
@@ -69,52 +69,73 @@ const ReviewPopup = ({ reviewId, closeReviewPopup }: ReviewPopupProps) => {
         <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
           <div
             ref={popupRef}
-            className="bg-white p-5 rounded-lg flex flex-col justify-between w-4/5 max-w-lg min-h-[400px]"
+            className="bg-white p-5 rounded-lg flex flex-col justify-between w-4/5 max-w-lg"
+            style={{ maxHeight: '500px' }}
           >
-            <div>
-              <button
-                onClick={closeReviewPopup}
-                className="px-2 py-1 text-white bg-blue-600 rounded hover:bg-blue-700 mb-5"
-              >
-                Back to reviews
-              </button>
-              <div className="flex justify-between text-lg">
+            <div style={{ marginBottom: '-55px' }}>
+              <div className="flex justify-between mt-2">
                 <div>
-                  <h2 className="font-bold">{review.reviewerUserName}</h2>
+                  <h2 className="font-bold text-lg">
+                    {review.reviewerUserName}
+                  </h2>
                 </div>
                 <div>
                   <p>{formatDateToDDMMYYYY(review.reviewCreatedAt)}</p>
                   <p>{format24HourTo12Hour(review.reviewCreatedAt)}</p>
                 </div>
               </div>
-              <div className="flex mt-8 items-center">
-                <img
-                  className="max-w-[150px]"
-                  src={review.productImage}
-                  alt={review.productName}
-                />
-                <h2>{review.productName}</h2>
+              <div className="flex mt-4 gap-4">
+                <div className="w-1/4">
+                  <img
+                    className="object-contain mx-auto mt-4"
+                    style={{
+                      maxHeight: '125px',
+                      maxWidth: '118px',
+                      marginTop: '11px',
+                    }}
+                    src={review.productImage}
+                    alt={review.productName}
+                  />
+                </div>
+                <div>
+                  <div style={{ maxWidth: '320px' }}>
+                    <div className="flex gap-4 mt-4">
+                      <p className="font-semibold">{`User's Rating:`}</p>
+                      <StarRating rating={review.reviewRating} size={1} />
+                      <p className="font-semibold">({review.reviewRating})</p>
+                    </div>
+
+                    <p>{review.reviewDescription}</p>
+                  </div>
+                </div>
               </div>
             </div>
-            <div>
-              <div className="flex gap-4 mt-4 mb-2">
-                <p className="font-bold">{`User's Rating:`}</p>
-                <StarRating rating={review.reviewRating} size={1} />
-                <p>({review.reviewRating})</p>
-              </div>
-              <h2 className="font-bold">Description:</h2>
-              <p>{review.reviewDescription}</p>
+            <hr
+              className="border-t border-gray-300"
+              style={{ marginTop: '90px' }}
+            />
+            <div className="flex justify-between items-center">
+              <button
+                onClick={() =>
+                  onToggle(review.reviewId, !review.reviewIsEnabled)
+                }
+                className="px-2 py-1 text-white rounded mt-8 mb-2 w-[80px]"
+                style={{
+                  backgroundColor: review.reviewIsEnabled ? 'green' : 'red',
+                }}
+              >
+                {review.reviewIsEnabled ? 'Enabled' : 'Disabled'}
+              </button>
+
+              <button
+                onClick={closeReviewPopup}
+                className="px-2 py-1 text-white bg-blue-600 rounded hover:bg-blue-700 mb-2 mt-8"
+              >
+                Back to reviews
+              </button>
             </div>
-            <button
-              onClick={() => onToggle(review.reviewId, !review.reviewIsEnabled)}
-              className="px-2 py-1 text-white rounded mt-7 w-[80px]"
-              style={{
-                backgroundColor: review.reviewIsEnabled ? 'green' : 'red',
-              }}
-            >
-              {review.reviewIsEnabled ? 'Enabled' : 'Disabled'}
-            </button>
           </div>
+          <div></div>
         </div>
       )}
     </>
