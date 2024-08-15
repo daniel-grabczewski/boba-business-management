c7b5b225d6e1d06f6a31086db8f137d2a7895ee1
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-16 10:47:10 +1200

    Build searchbar UI in SortFilterControls component for shopper product search

--- client/shopper/components/SortFilterControls/SortFilterControls.tsx
+++ client/shopper/components/SortFilterControls/SortFilterControls.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 interface SortFilterControlsProps {
-  filter: string;
-  sort: string;
-  handleChangeFilter: (value: string) => void;
-  handleChangeSort: (value: string) => void;
+  filter: string
+  sort: string
+  handleChangeFilter: (value: string) => void
+  handleChangeSort: (value: string) => void
+  search: string
+  handleChangeSearch: (search: string) => void
 }
 
 const SortFilterControls: React.FC<SortFilterControlsProps> = ({
@@ -12,44 +14,77 @@ const SortFilterControls: React.FC<SortFilterControlsProps> = ({
   sort,
   handleChangeFilter,
   handleChangeSort,
+  search,
+  handleChangeSearch,
 }) => {
   return (
-      <div style = {{marginTop : "20px", marginBottom : "60px"}}>
-        <label htmlFor="filter" className={'font-bold'}>Filter by: </label>
-        <select
-          name="filter"
-          id="filter"
-          onChange={(e) => handleChangeFilter(e.target.value)}
-          value={filter}
-          className="border rounded-md"
-        >
-          <option value="">...</option>
-          <option value="with-pearls">With pearls</option>
-          <option value="without-pearls">Without pearls</option>
-          <option value="teas">Teas</option>
-          <option value="smoothies">Smoothies</option>
-          <option value="yogurts">Yogurts</option>
-          <option value="fruit-drinks">Fruit drinks</option>
-          <option value="dairy-free">Dairy free</option>
-        </select>
-
-        <label htmlFor="sort" className="ml-4 font-bold" >
-          Sort by:{' '}
-        </label>
-        <select
-          name="sort"
-          id="sort"
-          onChange={(e) => handleChangeSort(e.target.value)}
-          value={sort}
-          className="border rounded-md"
-        >
-          <option value="">...</option>
-          <option value="price-low-to-high">Price (Low to High)</option>
-          <option value="price-high-to-low">Price (High to Low)</option>
-          <option value="a-z">Alphabetical (A to Z)</option>
-        </select>
+    <div
+      style={{
+        marginTop: '20px',
+        marginBottom: '40px',
+        minWidth: '1000px',
+        maxWidth: '1000px',
+      }}
+      className="flex items-center gap-4 bg-gray-200 p-4 rounded-md justify-between"
+    >
+      <input
+        style={{ maxWidth: '400px', minWidth: '400px' }}
+        className="border p-2 rounded w-full"
+        type="text"
+        placeholder="Search Product Name..."
+        value={search}
+        onChange={(e) => handleChangeSearch(e.target.value)}
+      />
+      <div className="flex gap-4">
+        <div>
+          <label
+            htmlFor="filter"
+            className={'font-bold'}
+            style={{ marginRight: '4px' }}
+          >
+            Filter by:{' '}
+          </label>
+          <select
+            name="filter"
+            id="filter"
+            onChange={(e) => handleChangeFilter(e.target.value)}
+            value={filter}
+            className="border rounded-md p-2 "
+          >
+            <option value="">...</option>
+            <option value="with-pearls">With pearls</option>
+            <option value="without-pearls">Without pearls</option>
+            <option value="teas">Teas</option>
+            <option value="smoothies">Smoothies</option>
+            <option value="yogurts">Yogurts</option>
+            <option value="fruit-drinks">Fruit drinks</option>
+            <option value="dairy-free">Dairy free</option>
+          </select>
+        </div>
+        <div>
+          <label
+            htmlFor="sort"
+            className="ml-4 font-bold"
+            style={{ marginRight: '4px' }}
+          >
+            Sort by:{' '}
+          </label>
+          <select
+            name="sort"
+            id="sort"
+            onChange={(e) => handleChangeSort(e.target.value)}
+            value={sort}
+            className="border rounded-md p-2"
+          >
+            <option value="">...</option>
+            <option value="price-low-to-high">Price (Low to High)</option>
+            <option value="price-high-to-low">Price (High to Low)</option>
+            <option value="a-z">Alphabetical (A to Z)</option>
+          </select>
+        </div>
       </div>
-  );
-};
+    </div>
+  )
+}
 
-export default SortFilterControls;
+export default SortFilterControls
