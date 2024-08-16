a2f98ac9acf80916aba0e57b3f9c859dff0aec57
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-16 14:28:55 +1200

    Remove admin nav bar from AppLayout

--- client/shopper/components/AppLayout/AppLayout.tsx
+++ client/shopper/components/AppLayout/AppLayout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import Nav from '../Nav/Nav'
 import Footer from '../Footer/Footer'
-import { AdminNavBar } from '../../../admin/components/AdminNavBar/AdminNavBar'
 
 function AppLayout() {
   return (
     <div className="flex flex-col min-h-screen">
-      <AdminNavBar />
       <Nav />
       <div className="flex-grow">
         <Outlet />
