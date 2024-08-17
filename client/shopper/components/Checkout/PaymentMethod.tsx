70a5109a725ac115ffab89aa8173dbbc7083bdd5
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-17 12:54:57 +1200

    Fix issue where not all selection fields in Checkout had matching borders

--- client/shopper/components/Checkout/PaymentMethod.tsx
+++ client/shopper/components/Checkout/PaymentMethod.tsx
@@ -5,7 +5,11 @@ function PaymentMethod() {
         <label htmlFor="payment" className="font-medium">
           SELECT PAYMENT METHOD
         </label>
-        <select name="payment" id="payment" className="border p-2 w-full">
+        <select
+          name="payment"
+          id="payment"
+          className="border p-2 w-full rounded-md"
+        >
           <option value="card">Debit Card</option>
           <option value="visa">PayPal</option>
           <option value="visa">Google Pay</option>
--- client/shopper/components/Checkout/ShippingMethod.tsx
+++ client/shopper/components/Checkout/ShippingMethod.tsx
@@ -10,23 +10,24 @@ function ShippingMethod({
 }: ShippingMethodProps) {
   return (
     <div className="w-full mr-6 mb-4 flex flex-row">
-      <div className="w-full mr-6"><label htmlFor="shipping" className="font-medium">
-        SELECT SHIPPING METHOD
-      </label>
-      <select
-        name="shipping"
-        id="shipping"
-        className="border p-2 w-full mb-4"
-        onChange={handleShippingChange}
-      >
-        {shippingOptions.map((option: ShippingOption) => (
-          <option value={option.id} key={option.id}>
-            {option.shippingType}
-          </option>
-        ))}
-      </select></div>
+      <div className="w-full mr-6">
+        <label htmlFor="shipping" className="font-medium">
+          SELECT SHIPPING METHOD
+        </label>
+        <select
+          name="shipping"
+          id="shipping"
+          className="border p-2 w-full mb-4 rounded-md"
+          onChange={handleShippingChange}
+        >
+          {shippingOptions.map((option: ShippingOption) => (
+            <option value={option.id} key={option.id}>
+              {option.shippingType}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className="w-full p-2 mb-4"></div>
-      
     </div>
   )
 }
