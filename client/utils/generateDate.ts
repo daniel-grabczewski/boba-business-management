28f2768a646670a18143eea9244d80d270c0f53a
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-20 13:17:47 +1200

    Handle case in getRandomDateTimeWithinLastDays function where the time generated can have a timestamp in the future of today's date

--- client/utils/generateDate.ts
+++ client/utils/generateDate.ts
@@ -24,22 +24,30 @@ export function generateCurrentDate(): string {
 }
 
 // Function to generate a random date within the last specified number of days in 'YYYY-MM-DD HH:MM:SS' format
+// Function to generate a random date within the last specified number of days in 'YYYY-MM-DD HH:MM:SS' format
 export function getRandomDateTimeWithinLastDays(days: number): string {
   const now = new Date()
-  const pastDate = new Date()
-  pastDate.setDate(now.getDate() - days)
-
-  // Generate a random date between pastDate and now
-  const randomDate = new Date(
-    pastDate.getTime() + Math.random() * (now.getTime() - pastDate.getTime())
+  const pastDate = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() - days,
+    6
   )
 
-  // Set a random time between 6am and 1am
-  const randomHour = Math.floor(Math.random() * (25 - 6) + 6) // Random hour between 6 and 24 (24 is 0 of next day)
-  const randomMinute = Math.floor(Math.random() * 60)
-  const randomSecond = Math.floor(Math.random() * 60)
+  // Generate a random date and time between pastDate and now
+  const randomTime =
+    pastDate.getTime() + Math.random() * (now.getTime() - pastDate.getTime())
+  const randomDate = new Date(randomTime)
 
-  randomDate.setHours(randomHour % 24, randomMinute, randomSecond)
+  // Constrain the time to be no later than the current time
+  if (randomDate > now) {
+    randomDate.setTime(now.getTime())
+  }
+
+  // Constrain the hours to be no earlier than 6 AM of each day
+  if (randomDate.getHours() < 6) {
+    randomDate.setHours(6)
+  }
 
   // Format the date to 'YYYY-MM-DD HH:MM:SS'
   const year = randomDate.getFullYear()
