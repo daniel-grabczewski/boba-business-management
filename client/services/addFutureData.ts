530779bd1c85fc635c7a4468201dcbc1099c2222
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-20 12:27:27 +1200

    Create addFutureData function in addFutureData.ts
    This function will add new data to the app on every day the user opens the app

--- client/services/addFutureData.ts
+++ client/services/addFutureData.ts
@@ -19,3 +19,93 @@ function cutArray<T extends { id: number }>(
 const minItems = 2
 const maxItems = 5
 
+export function addFutureData(): void {
+  const currentDate = formatDateToDDMMYYYY(generateCurrentDate())
+
+  const latestDateInStorage = localStorage.getItem('latestDateInStorage')
+
+  const latestIdsInStorage = localStorage.getItem('latestIdsInStorage')
+
+  // Initialise the data for the first time it is used
+  if (!latestDateInStorage || !latestIdsInStorage) {
+    localStorage.setItem(
+      'latestDateInStorage',
+      JSON.stringify({ currentDate: currentDate })
+    )
+
+    const orderCount = generateRandomNumber(minItems, maxItems)
+    const reviewCount = generateRandomNumber(minItems, maxItems)
+    const emailCount = generateRandomNumber(minItems, maxItems)
+
+    localStorage.setItem(
+      'latestIdsInStorage',
+      JSON.stringify({
+        latestOrderId: orderCount,
+        latestReviewId: reviewCount,
+        latestEmailId: emailCount,
+      })
+    )
+
+    const selectedFutureOrders = cutArray(futureOrders, 1, orderCount)
+    const selectedFutureReviews = cutArray(futureReviews, 1, reviewCount)
+    const selectedFutureEmails = cutArray(futureEmails, 1, emailCount)
+
+    //Function to process selectedFutureOrders will go here
+    //Function to process selectedFutureReviews will go here
+    //Function to process selectedFutureEmails will go here
+
+    //There is nothing else to do in this function, so we leave
+    return
+  }
+
+  //Otherwise, we use the latest Ids we set as the start point, we generate the numbers again, use them to cut them up, set the numbers as the latest IDs, and then process the cut up arrays into the functions
+
+  const latestDate = JSON.parse(latestDateInStorage).currentDate
+
+  //If the date in local storage is the same as the current day, do nothing
+  if (currentDate === latestDate) {
+    return
+  }
+
+  //Otherwise, we cut up the arrays
+  const latestIds = JSON.parse(latestIdsInStorage)
+
+  const orderCount = generateRandomNumber(minItems, maxItems)
+  const reviewCount = generateRandomNumber(minItems, maxItems)
+  const emailCount = generateRandomNumber(minItems, maxItems)
+
+  const selectedFutureOrders = cutArray(
+    futureOrders,
+    latestIds.latestOrderId + 1,
+    latestIds.latestOrderId + orderCount
+  )
+  const selectedFutureReviews = cutArray(
+    futureReviews,
+    latestIds.latestReviewId + 1,
+    latestIds.latestReviewId + reviewCount
+  )
+  const selectedFutureEmails = cutArray(
+    futureEmails,
+    latestIds.latestEmailId + 1,
+    latestIds.latestEmailId + emailCount
+  )
+
+  //Function to process selectedFutureOrders will go here
+  //Function to process selectedFutureReviews will go here
+  //Function to process selectedFutureEmails will go here
+
+  localStorage.setItem(
+    'latestIdsInStorage',
+    JSON.stringify({
+      latestOrderId: latestIds.latestOrderId + orderCount,
+      latestReviewId: latestIds.latestReviewId + reviewCount,
+      latestEmailId: latestIds.latestEmailId + emailCount,
+    })
+  )
+
+  //Set local storage date to current date, so when this function is run again, it won't get past the if statement 'currentDate === latestDate' check
+  localStorage.setItem(
+    'latestDateInStorage',
+    JSON.stringify({ currentDate: currentDate })
+  )
+}
