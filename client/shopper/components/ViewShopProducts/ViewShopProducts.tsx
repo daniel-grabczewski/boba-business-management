c80487d1884e16ebfcf9d7ddfa44ec5d6d618cae
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-26 11:30:14 +1200

    Modify ViewShopProducts component to show available stock of each item, taking into consideration the quantity of the item in the user's cart

--- client/shopper/components/ViewShopProducts/ViewShopProducts.tsx
+++ client/shopper/components/ViewShopProducts/ViewShopProducts.tsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom'
 import StarRating from '../../components/StarRating/StarRating'
 import { ShopperProduct } from '../../../../models/Products'
 import { lowStockThreshold } from '../../../data/lowStockThreshold'
+import { getAvailableStockByProductId } from '../../../services/cart'
+import { useMemo } from 'react'
 
 interface ViewShopProductsProps {
   hoveredProductId: number | null
@@ -14,9 +16,16 @@ const ViewShopProducts = ({
   setHoveredProductId,
   getPaginatedProducts,
 }: ViewShopProductsProps) => {
+  const productsWithStock = useMemo(() => {
+    return getPaginatedProducts().map((product) => ({
+      ...product,
+      availableStock: getAvailableStockByProductId(product.id),
+    }))
+  }, [getPaginatedProducts])
+
   return (
     <>
-      {getPaginatedProducts().length === 0 ? (
+      {productsWithStock.length === 0 ? (
         <div className="flex justify-center">
           <p className="font-semibold text-lg">
             No products matched your search
@@ -24,97 +33,99 @@ const ViewShopProducts = ({
         </div>
       ) : (
         <div className="grid grid-cols-3 gap-4">
-          {getPaginatedProducts().map((product) => (
-            <div
-              key={product.id}
-              className="border p-4 rounded-md flex flex-col flex-top justify-between"
-              style={{ width: '320px' }}
-            >
-              <div>
-                <Link
-                  to={`/shop/${product.id}`}
-                  className="w-full h-48 block"
-                  style={{ marginBottom: '15px' }}
-                >
-                  <img
-                    src={product.image}
-                    alt={product.name}
-                    className="w-full h-48 object-contain"
-                  />
-                </Link>
+          {productsWithStock.map((product) => {
+            return (
+              <div
+                key={product.id}
+                className="border p-4 rounded-md flex flex-col flex-top justify-between"
+                style={{ width: '320px' }}
+              >
+                <div>
+                  <Link
+                    to={`/shop/${product.id}`}
+                    className="w-full h-48 block"
+                    style={{ marginBottom: '15px' }}
+                  >
+                    <img
+                      src={product.image}
+                      alt={product.name}
+                      className="w-full h-48 object-contain"
+                    />
+                  </Link>
 
-                <Link
-                  to={`/shop/${product.id}`}
-                  onMouseEnter={() => setHoveredProductId(product.id)}
-                  onMouseLeave={() => setHoveredProductId(null)}
-                  className="text-xl font-bold mt-2 block cursor-pointer transition-all 300s"
-                  style={{
-                    color:
-                      hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
-                  }}
-                >
-                  {product.name}
-                </Link>
+                  <Link
+                    to={`/shop/${product.id}`}
+                    onMouseEnter={() => setHoveredProductId(product.id)}
+                    onMouseLeave={() => setHoveredProductId(null)}
+                    className="text-xl font-bold mt-2 block cursor-pointer transition-all 300s"
+                    style={{
+                      color:
+                        hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
+                    }}
+                  >
+                    {product.name}
+                  </Link>
 
-                <Link
-                  to={`/shop/${product.id}`}
-                  onMouseEnter={() => setHoveredProductId(product.id)}
-                  onMouseLeave={() => setHoveredProductId(null)}
-                  className="text-lg text-gray-600 block cursor-pointer transition-all 300s"
-                  style={{
-                    color:
-                      hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
-                  }}
-                >
-                  {' '}
-                  <div className="flex justify-between">
-                    <p>${product.price.toFixed(2)}</p>
-                  </div>
-                </Link>
-              </div>
-              <div>
-                <Link
-                  to={`/shop/${product.id}`}
-                  className="block cursor-pointer"
-                >
-                  <div className="flex justify-between">
-                    <div className="flex items-center mt-2 ">
-                      {product.averageRating === 0 ? (
-                        <p
-                          className="text-gray-500"
-                          style={{ marginBottom: '9px' }}
-                        >
-                          No reviews yet
-                        </p>
-                      ) : (
-                        <>
-                          <span className="text-yellow-400">
-                            <StarRating
-                              rating={product.averageRating}
-                              size={1.5}
-                            />
-                          </span>
-                          <span className="ml-2 text-sm text-gray-500">
-                            ({product.averageRating})
-                          </span>
-                        </>
-                      )}
+                  <Link
+                    to={`/shop/${product.id}`}
+                    onMouseEnter={() => setHoveredProductId(product.id)}
+                    onMouseLeave={() => setHoveredProductId(null)}
+                    className="text-lg text-gray-600 block cursor-pointer transition-all 300s"
+                    style={{
+                      color:
+                        hoveredProductId === product.id ? '#1D4ED8' : 'inherit',
+                    }}
+                  >
+                    {' '}
+                    <div className="flex justify-between">
+                      <p>${product.price.toFixed(2)}</p>
                     </div>
-                    <p
-                      style={{ fontSize: '15px' }}
-                      className="text-red-500 mt-4"
-                    >
-                      {product.stock === 0
-                        ? 'Out of stock'
-                        : product.stock <= lowStockThreshold
-                        ? `${product.stock} left in stock`
-                        : ''}
-                    </p>
-                  </div>
-                </Link>
+                  </Link>
+                </div>
+                <div>
+                  <Link
+                    to={`/shop/${product.id}`}
+                    className="block cursor-pointer"
+                  >
+                    <div className="flex justify-between">
+                      <div className="flex items-center mt-2 ">
+                        {product.averageRating === 0 ? (
+                          <p
+                            className="text-gray-500"
+                            style={{ marginBottom: '9px' }}
+                          >
+                            No reviews yet
+                          </p>
+                        ) : (
+                          <>
+                            <span className="text-yellow-400">
+                              <StarRating
+                                rating={product.averageRating}
+                                size={1.5}
+                              />
+                            </span>
+                            <span className="ml-2 text-sm text-gray-500">
+                              ({product.averageRating})
+                            </span>
+                          </>
+                        )}
+                      </div>
+                      <p
+                        style={{ fontSize: '15px' }}
+                        className="text-red-500 mt-4"
+                      >
+                        {product.availableStock === 0
+                          ? 'Out of stock'
+                          : product.availableStock <= lowStockThreshold
+                          ? `${product.availableStock} left in stock`
+                          : ''}
+                      </p>
+                    </div>
+                  </Link>
+                </div>
               </div>
-            </div>
-          ))}
+            )
+          })}
         </div>
       )}
     </>
