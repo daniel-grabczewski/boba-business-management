39fb7aa8c0a0be57b9980a7d5e58bfe400291ee0
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-27 10:09:01 +1200

    Create basic component with DaisyUI for setting a star rating

---
+++ client/UI/SetStarRating.tsx
@@ -0,0 +1,60 @@
+const SetStarRating = () => {
+  return (
+    <div className="rating rating-lg rating-half">
+      <input type="radio" name="rating-10" className="rating-hidden" />
+      <input
+        type="radio"
+        name="rating-10"
+        className="mask mask-star-2 mask-half-1 bg-yellow-400"
+      />
+      <input
+        type="radio"
+        name="rating-10"
+        className="mask mask-star-2 mask-half-2 bg-yellow-400"
+      />
+      <input
+        type="radio"
+        name="rating-10"
+        className="mask mask-star-2 mask-half-1 bg-yellow-400"
+        defaultChecked
+      />
+      <input
+        type="radio"
+        name="rating-10"
+        className="mask mask-star-2 mask-half-2 bg-yellow-400"
+      />
+      <input
+        type="radio"
+        name="rating-10"
+        className="mask mask-star-2 mask-half-1 bg-yellow-400"
+      />
+      <input
+        type="radio"
+        name="rating-10"
+        className="mask mask-star-2 mask-half-2 bg-yellow-400"
+      />
+      <input
+        type="radio"
+        name="rating-10"
+        className="mask mask-star-2 mask-half-1 bg-yellow-400"
+      />
+      <input
+        type="radio"
+        name="rating-10"
+        className="mask mask-star-2 mask-half-2 bg-yellow-400"
+      />
+      <input
+        type="radio"
+        name="rating-10"
+        className="mask mask-star-2 mask-half-1 bg-yellow-400"
+      />
+      <input
+        type="radio"
+        name="rating-10"
+        className="mask mask-star-2 mask-half-2 bg-yellow-400"
+      />
+    </div>
+  )
+}
+
+export default SetStarRating
