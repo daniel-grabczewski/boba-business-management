a7936dad84dd317f6913125c8489083959207554
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-27 11:37:12 +1200

    Improve styling for edit profile page

--- client/shopper/pages/Profile/EditProfile.tsx
+++ client/shopper/pages/Profile/EditProfile.tsx
@@ -121,233 +121,243 @@ const EditProfile = () => {
   }
 
   return (
-    <div className="w-1/2 mx-auto p-4 bg-gray-100 rounded shadow-lg mt-4">
-      <LoadError status={status} />
-      <h2 className="text-2xl text-center mb-4">Edit Profile</h2>
+    <div className="flex justify-center items-center h-full">
+      <div className="w-1/2 mx-auto p-4 bg-gray-100 rounded shadow-lg">
+        <LoadError status={status} />
+        <h2 className="text-2xl text-center mb-4 font-semibold">
+          Edit Profile
+        </h2>
 
-      <form onSubmit={handleSubmit}>
-        <div className="flex w-full gap-4 mb-4 ">
-          <div className="w-full">
-            <label htmlFor="firstName" className="block font-semibold mb-1">
-              First Name:
-            </label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              value={formData.firstName}
-              onChange={(e) => handleFieldChange(e, 'letters')}
-              className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
-                emptyFields.includes('firstName') ? 'border-red-500' : ''
-              } ${
-                invalidFields.includes('firstName')
-                  ? 'border-red-500 text-red-500'
-                  : ''
+        <form onSubmit={handleSubmit}>
+          <div className="flex w-full gap-4 mb-4 ">
+            <div className="w-full">
+              <label htmlFor="firstName" className="block font-semibold mb-1">
+                First Name:
+              </label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={formData.firstName}
+                onChange={(e) => handleFieldChange(e, 'letters')}
+                className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
+                  emptyFields.includes('firstName') ? 'border-red-500' : ''
+                } ${
+                  invalidFields.includes('firstName')
+                    ? 'border-red-500 text-red-500'
+                    : ''
+                }`}
+              />
+              <p
+                className={`text-sm ${
+                  invalidFields.includes('firstName')
+                    ? 'text-red-500'
+                    : 'invisible'
+                }`}
+              >
+                Please enter a valid first name
+              </p>
+            </div>
+
+            <div className="w-full">
+              <label htmlFor="lastName" className="block font-semibold mb-1">
+                Last Name:
+              </label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={formData.lastName}
+                onChange={(e) => handleFieldChange(e, 'letters')}
+                className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
+                  emptyFields.includes('lastName') ? 'border-red-500' : ''
+                } ${
+                  invalidFields.includes('lastName')
+                    ? 'border-red-500 text-red-500'
+                    : ''
+                }`}
+              />
+              <p
+                className={`text-sm ${
+                  invalidFields.includes('lastName')
+                    ? 'text-red-500'
+                    : 'invisible'
+                }`}
+              >
+                Please enter a valid last name
+              </p>
+            </div>
+          </div>
+
+          <div className=" w-full flex gap-4">
+            <div className="w-full">
+              <label htmlFor="phoneNumber" className="block font-semibold mb-1">
+                Phone Number:
+              </label>
+              <input
+                type="text"
+                id="phoneNumber"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={(e) => handleFieldChange(e, 'numbers')}
+                className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
+                  emptyFields.includes('phoneNumber') ? 'border-red-500' : ''
+                } ${
+                  invalidFields.includes('phoneNumber')
+                    ? 'border-red-500 text-red-500'
+                    : ''
+                }`}
+              />
+              <p
+                className={`text-sm ${
+                  invalidFields.includes('phoneNumber')
+                    ? 'text-red-500'
+                    : 'invisible'
+                }`}
+              >
+                Please enter a valid phone number
+              </p>
+            </div>
+            <div className="w-full"></div>
+          </div>
+          <div className="flex gap-4 mb-4">
+            <div className="w-full">
+              <label htmlFor="address" className="block font-semibold mb-1">
+                Address:
+              </label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                value={formData.address}
+                onChange={handleFieldChange}
+                className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
+                  emptyFields.includes('address') ? 'border-red-500' : ''
+                }`}
+              />
+              <p className="invisible text-sm">Placeholder</p>
+            </div>
+
+            <div className="w-full">
+              <label htmlFor="city" className="block font-semibold mb-1">
+                City:
+              </label>
+              <input
+                type="text"
+                id="city"
+                name="city"
+                value={formData.city}
+                onChange={(e) => handleFieldChange(e, 'letters')}
+                className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
+                  emptyFields.includes('city') ? 'border-red-500' : ''
+                } ${
+                  invalidFields.includes('city')
+                    ? 'border-red-500 text-red-500'
+                    : ''
+                }`}
+              />
+
+              <p
+                className={`text-sm ${
+                  invalidFields.includes('city') ? 'text-red-500' : 'invisible'
+                }`}
+              >
+                Please enter a valid city
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-4 mb-8">
+            <div className="w-full">
+              <label htmlFor="country" className="block font-semibold mb-1">
+                Country:
+              </label>
+              <input
+                type="text"
+                id="country"
+                name="country"
+                value={formData.country}
+                onChange={(e) => handleFieldChange(e, 'letters')}
+                className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
+                  emptyFields.includes('country') ? 'border-red-500' : ''
+                } ${
+                  invalidFields.includes('country')
+                    ? 'border-red-500 text-red-500'
+                    : ''
+                }`}
+              />
+              <p
+                className={`text-sm ${
+                  invalidFields.includes('country')
+                    ? 'text-red-500'
+                    : 'invisible'
+                }`}
+              >
+                Please enter a valid country
+              </p>
+            </div>
+
+            <div className="w-full">
+              <label htmlFor="zipCode" className="block font-semibold mb-1">
+                Zip Code:
+              </label>
+              <input
+                type="text"
+                id="zipCode"
+                name="zipCode"
+                value={formData.zipCode}
+                onChange={(e) => handleFieldChange(e, 'numbers')}
+                className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
+                  emptyFields.includes('zipCode') ? 'border-red-500' : ''
+                } ${
+                  invalidFields.includes('zipCode')
+                    ? 'border-red-500 text-red-500'
+                    : ''
+                }`}
+              />
+              <p
+                className={`text-sm ${
+                  invalidFields.includes('zipCode')
+                    ? 'text-red-500'
+                    : 'invisible'
+                }`}
+              >
+                Please enter a valid zip code
+              </p>
+            </div>
+          </div>
+
+          <div className="items-center flex flex-col">
+            <button
+              type="submit"
+              className={`w-1/6 py-2 px-4 text-white font-semibold rounded focus:outline-none focus:ring transition-all duration-300 ${
+                mutation.isLoading
+                  ? 'bg-gray-300 cursor-not-allowed'
+                  : 'bg-blue-500 hover:bg-blue-700'
               }`}
-            />
+            >
+              Submit
+            </button>
             <p
               className={`text-sm ${
-                invalidFields.includes('firstName')
+                showError && emptyFields.length > 0
                   ? 'text-red-500'
                   : 'invisible'
               }`}
             >
-              Please enter a valid first name
+              Please fill all fields
             </p>
-          </div>
-
-          <div className="w-full">
-            <label htmlFor="lastName" className="block font-semibold mb-1">
-              Last Name:
-            </label>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              value={formData.lastName}
-              onChange={(e) => handleFieldChange(e, 'letters')}
-              className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
-                emptyFields.includes('lastName') ? 'border-red-500' : ''
-              } ${
-                invalidFields.includes('lastName')
-                  ? 'border-red-500 text-red-500'
-                  : ''
-              }`}
-            />
             <p
               className={`text-sm ${
-                invalidFields.includes('lastName')
+                showError && invalidFields.length > 0
                   ? 'text-red-500'
                   : 'invisible'
               }`}
             >
-              Please enter a valid last name
+              Please correct the invalid inputs
             </p>
           </div>
-        </div>
-
-        <div className=" w-full flex gap-4">
-          <div className="w-full">
-            <label htmlFor="phoneNumber" className="block font-semibold mb-1">
-              Phone Number:
-            </label>
-            <input
-              type="text"
-              id="phoneNumber"
-              name="phoneNumber"
-              value={formData.phoneNumber}
-              onChange={(e) => handleFieldChange(e, 'numbers')}
-              className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
-                emptyFields.includes('phoneNumber') ? 'border-red-500' : ''
-              } ${
-                invalidFields.includes('phoneNumber')
-                  ? 'border-red-500 text-red-500'
-                  : ''
-              }`}
-            />
-            <p
-              className={`text-sm ${
-                invalidFields.includes('phoneNumber')
-                  ? 'text-red-500'
-                  : 'invisible'
-              }`}
-            >
-              Please enter a valid phone number
-            </p>
-          </div>
-          <div className="w-full"></div>
-        </div>
-        <div className="flex gap-4 mb-4">
-          <div className="w-full">
-            <label htmlFor="address" className="block font-semibold mb-1">
-              Address:
-            </label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              value={formData.address}
-              onChange={handleFieldChange}
-              className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
-                emptyFields.includes('address') ? 'border-red-500' : ''
-              }`}
-            />
-            <p className="invisible text-sm">Placeholder</p>
-          </div>
-
-          <div className="w-full">
-            <label htmlFor="city" className="block font-semibold mb-1">
-              City:
-            </label>
-            <input
-              type="text"
-              id="city"
-              name="city"
-              value={formData.city}
-              onChange={(e) => handleFieldChange(e, 'letters')}
-              className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
-                emptyFields.includes('city') ? 'border-red-500' : ''
-              } ${
-                invalidFields.includes('city')
-                  ? 'border-red-500 text-red-500'
-                  : ''
-              }`}
-            />
-
-            <p
-              className={`text-sm ${
-                invalidFields.includes('city') ? 'text-red-500' : 'invisible'
-              }`}
-            >
-              Please enter a valid city
-            </p>
-          </div>
-        </div>
-        <div className="flex gap-4 mb-8">
-          <div className="w-full">
-            <label htmlFor="country" className="block font-semibold mb-1">
-              Country:
-            </label>
-            <input
-              type="text"
-              id="country"
-              name="country"
-              value={formData.country}
-              onChange={(e) => handleFieldChange(e, 'letters')}
-              className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
-                emptyFields.includes('country') ? 'border-red-500' : ''
-              } ${
-                invalidFields.includes('country')
-                  ? 'border-red-500 text-red-500'
-                  : ''
-              }`}
-            />
-            <p
-              className={`text-sm ${
-                invalidFields.includes('country') ? 'text-red-500' : 'invisible'
-              }`}
-            >
-              Please enter a valid country
-            </p>
-          </div>
-
-          <div className="w-full">
-            <label htmlFor="zipCode" className="block font-semibold mb-1">
-              Zip Code:
-            </label>
-            <input
-              type="text"
-              id="zipCode"
-              name="zipCode"
-              value={formData.zipCode}
-              onChange={(e) => handleFieldChange(e, 'numbers')}
-              className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-400 ${
-                emptyFields.includes('zipCode') ? 'border-red-500' : ''
-              } ${
-                invalidFields.includes('zipCode')
-                  ? 'border-red-500 text-red-500'
-                  : ''
-              }`}
-            />
-            <p
-              className={`text-sm ${
-                invalidFields.includes('zipCode') ? 'text-red-500' : 'invisible'
-              }`}
-            >
-              Please enter a valid zip code
-            </p>
-          </div>
-        </div>
-
-        <div className="items-center flex flex-col">
-          <button
-            type="submit"
-            className={`w-1/6 py-2 px-4 text-white font-semibold rounded focus:outline-none focus:ring transition-all duration-300 ${
-              mutation.isLoading
-                ? 'bg-gray-300 cursor-not-allowed'
-                : 'bg-blue-500 hover:bg-blue-700'
-            }`}
-          >
-            Submit
-          </button>
-          <p
-            className={`text-sm ${
-              showError && emptyFields.length > 0 ? 'text-red-500' : 'invisible'
-            }`}
-          >
-            Please fill all fields
-          </p>
-          <p
-            className={`text-sm ${
-              showError && invalidFields.length > 0
-                ? 'text-red-500'
-                : 'invisible'
-            }`}
-          >
-            Please correct the invalid inputs
-          </p>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
