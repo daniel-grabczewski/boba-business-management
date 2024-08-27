ca8d27fdf4f8a9302bc6dfa4f23d52debc627603
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-28 11:51:56 +1200

    Create single source for all miscellaneous image file paths with dynamic baseURL

---
+++ client/data/miscImages.ts
@@ -0,0 +1,7 @@
+import { baseURL } from '../../baseUrl'
+export const placeholderImage = `${baseURL}/images/placeholder-image.png`
+export const backgroundImage1 = `${baseURL}/images/ellipse1.svg`
+export const backgroundImage2 = `${baseURL}/images/ellipse2.svg`
+export const homePageTeaImage = `${baseURL}/images/home-tea.svg`
+export const homePageTitleImage = `${baseURL}/images/home-title.svg`
+export const thankYouImage = `${baseURL}/images/thank-you.svg`
