092390c1864e659381b405d25a6fd25aed05afda
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-08-29 09:43:15 +1200

    Refactor vite.config file

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,26 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
-import EnvironmentPlugin from 'vite-plugin-environment'
-import dotenv from 'dotenv'
-
-console.log('NODE_ENV:', process.env.NODE_ENV)
-console.log('VITE_APP_BASE_URL:', process.env.VITE_APP_BASE_URL)
-
-// Load the correct environment variables
-dotenv.config({
-  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
-})
 
 export default defineConfig({
-  base: process.env.VITE_APP_BASE_URL || '/',
+  base:
+    process.env.NODE_ENV === 'production' ? '/boba-business-management/' : '/',
   plugins: [
     react(),
     viteStaticCopy({
       targets: [{ src: '404.html', dest: '.' }],
-    }),
-    EnvironmentPlugin({
-      NODE_ENV: process.env.NODE_ENV,
     }),
   ],
   server: {
