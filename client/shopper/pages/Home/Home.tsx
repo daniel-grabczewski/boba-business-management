f5486d48f0c7bf13ef553397053b582d6171b3e5
Daniel <danielgrabczewski@gmail.com>
2024-09-10 15:56:04 +1200

    mobile view in progress...

--- client/shopper/pages/Home/Home.tsx
+++ client/shopper/pages/Home/Home.tsx
@@ -7,57 +7,61 @@ import { baseURL } from '../../../../baseUrl'
 
 const Home = () => {
   const navigate = useNavigate()
+
   function goTo(link: string) {
     navigate(`${baseURL}${link}`)
   }
+
   // Scroll to top when the component is mounted
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
+
   return (
-    <div className="bg-home-rose">
-      <div
-        className="flex items-center justify-center"
-        style={{
-          backgroundImage: `url('${backgroundImage1}'), url('${backgroundImage2}')`,
-          backgroundPosition: 'bottom left, top right',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: '88vh, 80vh',
-          height: '95vh',
-        }}
-      >
-        <div
-          className="flex items-center justify-center"
-          style={{ marginTop: '-50px' }}
-        >
-          <div className=" flex flex-row items-center justify-center mx-auto">
-            {/* Welcome Card */}
-            <div
-              style={{ width: '38vh', marginLeft: '30vh' }}
-              className="w-1/2"
-            >
+    <>
+      <div className="fixed bg-home-rose min-w-screen min-h-screen relative">
+        {/* Background images */}
+        <div className="inset-0 z-0">
+          <div
+            className="absolute top-0 right-0 w-1/2 h-1/2 bg-no-repeat overflow-show "
+            style={{
+              backgroundImage: `url('${backgroundImage2}')`,
+            }}
+          />
+          <div
+            className="absolute bottom-0 left-0 w-full h-1/2 bg-no-repeat"
+            style={{
+              backgroundImage: `url('${backgroundImage1}')`,
+            }}
+          />
+        </div>
+
+        {/* Main content */}
+        <div className="relative z-10 flex items-center justify-center min-h-screen max-w-screen-lg mx-auto p-6 overflow-y-auto">
+          <div className="flex flex-col md:flex-row items-center justify-center mx-auto space-y-6 md:space-y-0 md:space-x-8">
+            {/* Boba Image */}
+            <div className="w-2/3 md:w-1/3 lg:w-1/4">
               <img
                 src={homePageTeaImage}
-                alt="Boba Buddies Logo"
-                className="max-w-full h-auto pr-10"
+                alt="Boba Buddies Drink"
+                className="w-full h-auto"
               />
             </div>
 
-            <div className="flex flex-col text-white text-xl p-10 w-1/2">
+            {/* Text and Button */}
+            <div className="flex flex-col text-white text-lg md:text-xl lg:text-2xl w-full md:w-1/2 lg:w-1/2 items-center md:items-start text-center md:text-left space-y-4">
               <img
                 src={homePageTitleImage}
                 alt="Boba Buddies Logo"
-                className="max-w-full h-auto"
-                style={{ width: '47vh' }}
+                className="w-3/4 md:w-full h-auto"
               />
-              <p className="text-2xl w-3/4 mt-4">
+              <p className="text-lg md:text-xl lg:text-2xl">
                 Dive into our colorful world of delicious flavors, hand-shaken
                 to perfection. Join the fun, and become a boba buddy today!
               </p>
               <button
                 onClick={() => goTo('/shop')}
-                className="w-1/3 border border-white text-rose-300 bg-white px-6 py-2 rounded-2xl hover:bg-grape hover:text-white hover:border-grape transition-all duration-300 mt-8 "
-                style={{ borderWidth: '3px' }}
+                className="w-2/3 md:w-1/3 border-4 border-white text-rose-300 bg-white px-6 py-2 rounded-2xl hover:bg-grape hover:text-white hover:border-grape transition-all duration-300"
               >
                 Shop
               </button>
@@ -65,7 +69,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
