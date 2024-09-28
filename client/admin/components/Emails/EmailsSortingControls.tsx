d52e8bf4492e2411a63630e8e5d01bb6a91e5bcc
Daniel <danielgrabczewski@gmail.com>
2024-09-29 12:21:49 +1300

    Make EmailsSortingControls.tsx mobile responsive

--- client/admin/components/Emails/EmailsSortingControls.tsx
+++ client/admin/components/Emails/EmailsSortingControls.tsx
@@ -25,73 +25,79 @@ const EmailsSortingControls: React.FC<EmailSortingControlsProps> = ({
 }) => {
   const lastIndex = page * 10
   const firstIndex = lastIndex - 10
+
   return (
-    <div className="border p-2 rounded flex flex-row justify-between items-center">
-      <div className="flex items-center">
+    <div className="border p-2 rounded flex flex-col sm:flex-row justify-between items-center mb-4 bg-white sticky top-0 z-10">
+      <div className="flex flex-col sm:flex-row items-start sm:items-center w-full sm:w-auto">
         {/* SEARCH */}
         <input
-          className="border p-2 rounded mr-2"
+          className="border p-2 rounded mb-2 sm:mb-0 sm:mr-2 w-full sm:w-auto"
           type="text"
-          placeholder="Search for Email..."
+          placeholder="Search Email..."
           value={search}
           onChange={(e) => {
             handleChangeSearch(e.target.value)
             handleChangePage(1)
           }}
         />
-        {/* FILTER */}
-        <p className="mx-2 font-semibold">Filter by:</p>
-        <select
-          className="border p-2 rounded"
-          onChange={(e) => handleChangeFilter(e.target.value)}
-          value={filter}
-        >
-          <option value="all">All emails</option>
-          <option value="unread">Unread emails</option>
-        </select>
 
-        {/* SORT */}
-        <p className="mx-2 font-semibold">Sort by:</p>
-        <select
-          className="border p-2 rounded"
-          onChange={(e) => handleChangeSort(e.target.value)}
-          value={sort}
-        >
-          <option value="newest-first">Newest first</option>
-          <option value="oldest-first">Oldest first</option>
-        </select>
+        {/* FILTER */}
+        <div className="flex flex-wrap items-center w-full sm:w-auto">
+          <p className="mx-2 font-semibold whitespace-nowrap">Filter by:</p>
+          <select
+            className="border p-2 rounded w-full sm:w-auto"
+            onChange={(e) => handleChangeFilter(e.target.value)}
+            value={filter}
+          >
+            <option value="all">All emails</option>
+            <option value="unread">Unread emails</option>
+          </select>
+
+          {/* SORT */}
+          <p className="mx-2 font-semibold whitespace-nowrap mt-2 sm:mt-0">
+            Sort by:
+          </p>
+          <select
+            className="border p-2 rounded w-full sm:w-auto"
+            onChange={(e) => handleChangeSort(e.target.value)}
+            value={sort}
+          >
+            <option value="newest-first">Newest first</option>
+            <option value="oldest-first">Oldest first</option>
+          </select>
+        </div>
       </div>
 
       {/* PAGINATION */}
-      <div className="flex justify-between items-center">
-        <div className="flex flex-col justify-center mx-2 font-semibold">
-          Showing {firstIndex + 1}-{Math.min(lastIndex, sortedEmailsCount)} of{' '}
-          {sortedEmailsCount}
+      <div className="flex justify-between items-center w-full sm:w-auto mt-2 sm:mt-0">
+        <div className="flex flex-col justify-end mx-2 font-semibold w-full sm:w-auto">
+          <p className="text-left sm:text-right">
+            Showing {sortedEmailsCount === 0 ? 0 : firstIndex + 1}-
+            {Math.min(lastIndex, sortedEmailsCount)} of {sortedEmailsCount}
+          </p>
         </div>
-        <div className="flex justify-center">
-          <button
-            className={`${
-              page === 1
-                ? 'bg-gray-300 cursor-default'
-                : 'bg-blue-500 hover:bg-blue-700'
-            } text-white font-bold py-2 px-4 rounded transition-all duration-300`}
-            disabled={page === 1}
-            onClick={() => handleChangePage(page - 1)}
-          >
-            {'<'}
-          </button>
-          <button
-            className={`${
-              page === totalPages
-                ? 'bg-gray-300 cursor-default'
-                : 'bg-blue-500 hover:bg-blue-700'
-            } text-white font-bold py-2 px-4 rounded ml-2 transition-all duration-300`}
-            disabled={page === totalPages}
-            onClick={() => handleChangePage(page + 1)}
-          >
-            {'>'}
-          </button>
-        </div>
+        <button
+          className={`${
+            page === 1
+              ? 'bg-gray-300 cursor-default'
+              : 'bg-blue-500 hover:bg-blue-700'
+          } text-white font-bold py-2 px-4 rounded transition-all duration-300`}
+          disabled={page === 1}
+          onClick={() => handleChangePage(page - 1)}
+        >
+          {'<'}
+        </button>
+        <button
+          className={`${
+            page === totalPages || sortedEmailsCount === 0
+              ? 'bg-gray-300 cursor-default'
+              : 'bg-blue-500 hover:bg-blue-700'
+          } text-white font-bold py-2 px-4 rounded ml-2 transition-all duration-300`}
+          disabled={page === totalPages || sortedEmailsCount === 0}
+          onClick={() => handleChangePage(page + 1)}
+        >
+          {'>'}
+        </button>
       </div>
     </div>
   )
