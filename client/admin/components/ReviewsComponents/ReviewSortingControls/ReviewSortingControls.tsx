9349313640ffb4a02699b417010d7376bb9336e7
Daniel <danielgrabczewski@gmail.com>
2024-10-04 11:00:26 +1300

    Make sorting controls responsive

--- client/admin/components/ReviewsComponents/ReviewSortingControls/ReviewSortingControls.tsx
+++ client/admin/components/ReviewsComponents/ReviewSortingControls/ReviewSortingControls.tsx
@@ -27,11 +27,11 @@ const ReviewSortingControls: React.FC<ReviewSortingControlsProps> = ({
   const firstIndex = lastIndex - 10
 
   return (
-    <div className="border p-2 rounded flex flex-col sm:flex-row justify-between items-center mb-4 bg-white sticky top-0 z-10">
-      <div className="flex flex-col sm:flex-row items-start sm:items-center w-full sm:w-auto">
+    <div className="border p-2 rounded flex flex-col custom:flex-row justify-between items-center mb-4 bg-white sticky top-0 z-10">
+      <div className="flex flex-col custom:flex-row items-start custom:items-center w-full custom:w-auto">
         {/* SEARCH */}
         <input
-          className="border p-2 rounded mb-2 sm:mb-0 sm:mr-2 w-full sm:w-auto"
+          className="border p-2 rounded mb-2 custom:mb-0 custom:mr-2 w-full custom:w-auto text-sm custom:text-base"
           type="text"
           placeholder="Search Review or User..."
           value={search}
@@ -42,10 +42,12 @@ const ReviewSortingControls: React.FC<ReviewSortingControlsProps> = ({
         />
 
         {/* FILTER */}
-        <div className="flex flex-wrap items-center w-full sm:w-auto">
-          <p className="mx-2 font-semibold whitespace-nowrap">Filter by:</p>
+        <div className="flex flex-wrap items-center w-full custom:w-auto">
+          <p className="mx-2 font-semibold whitespace-nowrap text-sm custom:text-base">
+            Filter by:
+          </p>
           <select
-            className="border p-2 rounded w-full sm:w-auto"
+            className="border p-2 rounded w-full custom:w-auto text-sm custom:text-base"
             onChange={(e) => handleChangeFilter(e.target.value)}
             value={filter}
           >
@@ -55,11 +57,11 @@ const ReviewSortingControls: React.FC<ReviewSortingControlsProps> = ({
           </select>
 
           {/* SORT */}
-          <p className="mx-2 font-semibold whitespace-nowrap mt-2 sm:mt-0">
+          <p className="mx-2 font-semibold whitespace-nowrap mt-2 custom:mt-0 text-sm custom:text-base">
             Sort by:
           </p>
           <select
-            className="border p-2 rounded w-full sm:w-auto"
+            className="border p-2 rounded w-full custom:w-auto text-sm custom:text-base"
             onChange={(e) => handleChangeSort(e.target.value)}
             value={sort}
           >
@@ -76,9 +78,9 @@ const ReviewSortingControls: React.FC<ReviewSortingControlsProps> = ({
       </div>
 
       {/* PAGINATION */}
-      <div className="flex justify-between items-center w-full sm:w-auto mt-2 sm:mt-0">
-        <div className="flex flex-col justify-end mx-2 font-semibold w-full sm:w-auto">
-          <p className="text-left sm:text-right">
+      <div className="flex justify-between items-center w-full custom:w-auto mt-2 custom:mt-0">
+        <div className="flex flex-col justify-end mx-2 font-semibold w-full custom:w-auto text-sm custom:text-base">
+          <p className="text-left custom:text-right">
             Showing {reviewsCount === 0 ? 0 : firstIndex + 1}-
             {Math.min(lastIndex, reviewsCount)} of {reviewsCount}
           </p>
@@ -88,7 +90,7 @@ const ReviewSortingControls: React.FC<ReviewSortingControlsProps> = ({
             page === 1
               ? 'bg-gray-300 cursor-default'
               : 'bg-blue-500 hover:bg-blue-700'
-          } text-white font-bold py-2 px-4 rounded transition-all duration-300`}
+          } text-white font-bold py-2 px-4 rounded transition-all duration-300 text-sm custom:text-base`}
           disabled={page === 1}
           onClick={() => handleChangePage(page - 1)}
         >
@@ -99,7 +101,7 @@ const ReviewSortingControls: React.FC<ReviewSortingControlsProps> = ({
             page === totalPages || reviewsCount === 0
               ? 'bg-gray-300 cursor-default'
               : 'bg-blue-500 hover:bg-blue-700'
-          } text-white font-bold py-2 px-4 rounded ml-2 transition-all duration-300`}
+          } text-white font-bold py-2 px-4 rounded ml-2 transition-all duration-300 text-sm custom:text-base`}
           disabled={page === totalPages || reviewsCount === 0}
           onClick={() => handleChangePage(page + 1)}
         >
