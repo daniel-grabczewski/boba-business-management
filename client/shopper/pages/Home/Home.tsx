0d7b193e496e7d39f8e6ce0485e4045d457d94aa
Daniel <danielgrabczewski@gmail.com>
2024-10-17 13:44:31 +1300

    Update Home.tsx styling

--- client/shopper/pages/Home/Home.tsx
+++ client/shopper/pages/Home/Home.tsx
@@ -1,8 +1,11 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { backgroundImage1, backgroundImage2 } from '../../../data/miscImages'
-import { homePageTeaImage } from '../../../data/miscImages'
-import { homePageTitleImage } from '../../../data/miscImages'
+import {
+  backgroundImage1,
+  backgroundImage2,
+  homePageTeaImage,
+  homePageTitleImage,
+} from '../../../data/miscImages'
 import { baseURL } from '../../../../baseUrl'
 
 const Home = () => {
@@ -18,62 +21,57 @@ const Home = () => {
   }, [])
 
   return (
-    <>
-      <div className="bg-home-rose min-w-screen min-h-screen relative">
-        {/* Background images */}
-        <div className="inset-0 z-0">
-          <div
-            className="absolute bottom-0 left-0 w-full h-1/2 bg-no-repeat"
-            style={{
-              backgroundImage: `url('${backgroundImage1}')`,
-            }}
-          ></div>
-          <div
-            className="absolute top-0 right-0 bg-no-repeat"
-            style={{
-              width: window.innerWidth > 768 ? '50vw' : '80vw',
-              height: window.innerWidth > 768 ? '40vh' : '50vh',
-              backgroundImage: `url('${backgroundImage2}')`,
-              backgroundSize: 'contain',
-              backgroundPosition: 'top right',
-              backgroundRepeat: 'no-repeat',
-            }}
-          ></div>
-          {/* Main content */}
-          <div className="relative z-10 flex items-center justify-center min-h-screen max-w-screen-lg mx-auto p-6 overflow-y-auto">
-            <div className="flex flex-col sm:flex-row items-center justify-center mx-auto space-y-6 sm:space-y-0 sm:space-x-8">
-              {/* Boba Image */}
-              <div className="hidden md:flex justify-center">
-                <img
-                  src={homePageTeaImage}
-                  alt="Boba Buddies Drink"
-                  className="w-[40%] sm:w-[50%] md:w-[70%] h-auto"
-                />
-              </div>
+    <div className="bg-home-rose min-w-screen min-h-screen relative flex items-center justify-center">
+      {/* Background Images */}
+      <div
+        className="absolute bottom-0 left-0 w-full h-1/2 bg-no-repeat"
+        style={{ backgroundImage: `url(${backgroundImage1})` }}
+      />
+      <div
+        className="absolute top-0 right-0 bg-no-repeat"
+        style={{
+          width: window.innerWidth > 768 ? '50vw' : '80vw',
+          height: window.innerWidth > 768 ? '40vh' : '50vh',
+          backgroundImage: `url(${backgroundImage2})`,
+          backgroundSize: 'contain',
+          backgroundPosition: 'top right',
+          backgroundRepeat: 'no-repeat',
+        }}
+      />
 
-              {/* Text and Button */}
-              <div className="flex flex-col text-white text-lg md:text-xl lg:text-2xl w-full md:w-1/2 lg:w-1/2 items-center md:items-start text-center md:text-left space-y-4">
-                <img
-                  src={homePageTitleImage}
-                  alt="Boba Buddies Logo"
-                  className="w-2/3 md:w-full h-auto"
-                />
-                <p className="text-lg md:text-xl lg:text-2xl">
-                  Dive into our colorful world of delicious flavors, hand-shaken
-                  to perfection. Join the fun, and become a boba buddy today!
-                </p>
-                <button
-                  onClick={() => goTo('/shop')}
-                  className="w-2/3 md:w-1/3 border-4 border-white text-rose-300 bg-white px-6 py-2 rounded-2xl hover:bg-purple-700 hover:text-white hover:border-grape transition-all duration-300"
-                >
-                  Shop
-                </button>
-              </div>
-            </div>
+      {/* Main Content */}
+      <div className="relative z-10 max-w-screen-lg mx-auto p-6 md:p-10 w-full flex items-center justify-center">
+        <div className="flex flex-col sm:flex-row items-center justify-center w-full">
+          {/* Boba Image */}
+          <div className="hidden md:flex justify-center">
+            <img
+              src={homePageTeaImage}
+              alt="Boba Buddies Drink"
+              className="w-[40%] sm:w-[50%] md:w-[70%] h-auto"
+            />
+          </div>
+
+          {/* Text and Button Section */}
+          <div className="flex flex-col items-start text-white text-lg md:text-xl lg:text-2xl w-full md:w-1/2 lg:w-1/2 space-y-4">
+            <img
+              src={homePageTitleImage}
+              alt="Boba Buddies Logo"
+              className="w-2/3 md:w-full h-auto"
+            />
+            <p className="text-lg w-2/3 md:w-full md:text-xl lg:text-2xl">
+              Dive into our colorful world of delicious flavors, hand-shaken to
+              perfection. Join the fun, and become a boba buddy today!
+            </p>
+            <button
+              onClick={() => goTo('/shop')}
+              className="w-1/3 text-rose-300 bg-white px-6 py-2 rounded-2xl hover:bg-purple-700 hover:text-white transition-all duration-300"
+            >
+              Shop
+            </button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
