6e9f2b3b1485a6d4b71f77a18404da585168256a
daniel <danielgrabczewski@gmail.com>
2024-12-03 15:06:12 +1300

    Fix issues with padding on mobile view for Contact component

--- client/shopper/pages/Contact/Contact.tsx
+++ client/shopper/pages/Contact/Contact.tsx
@@ -76,7 +76,7 @@ function Contact() {
   }, [alertMessage])
 
   return (
-    <div className="mx-auto max-w-2xl  flex flex-col my-10">
+    <div className="mx-auto max-w-2xl  flex flex-col my-10 px-8">
       <div className="text-4xl text-center font-bold mb-6">Contact Us</div>
       <form className="space-y-4" onSubmit={handleSubmit}>
         <div>
@@ -105,6 +105,7 @@ function Contact() {
                 ? 'border-red-500 focus:ring-red-500'
                 : 'focus:ring-black'
             }`}
+            style={{maxHeight : '400px', minHeight : '100px'}}
             name="description"
             onChange={handleMessageChange}
             value={newEmail.description}
